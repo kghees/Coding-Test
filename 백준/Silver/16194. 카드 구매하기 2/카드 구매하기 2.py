5afadceac5f9d33775e1700977a3d1a578d60093
김건희 <92205960+kghees@users.noreply.github.com>
2023-06-29 22:46:45 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 144 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -1,8 +1,9 @@
 n = int(input())
 p = [0] + list(map(int,input().split()))
-d = [1000*10000]*(n+1)
+d = [-1]*(n+1)
 d[0] = 0
 for i in range(1, n+1):
   for j in range(1, i+1):
-    d[i] = min(d[i],d[i-j]+p[j])
+    if d[i] == -1 or d[i] > d[i-j] + p[j]:
+      d[i] = d[i-j]+p[j]
 print(d[i])
