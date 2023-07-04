0a165ca16519a2d3b81f89a27edde05cf6053f1b
김건희 <92205960+kghees@users.noreply.github.com>
2023-07-04 22:01:30 +0900

    [Silver I] Title: 동물원, Time: 100 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,14 +1,15 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-d = [0] * (n+1)
-for i in range(n+1):
-    d[i] = [0,0,0]
-d[1][0] = 1
-d[1][1] = 1
-d[1][2] = 1
-for i in range(2, n + 1):
-  d[i][0] = (d[i-1][0] + d[i-1][1] + d[i-1][2]) % 9901
-  d[i][1] = (d[i-1][0] + d[i-1][2]) % 9901
-  d[i][2] = (d[i-1][0] + d[i-1][1]) % 9901
-print(sum(d[n])%9901)
+d = [0]*(n+1)
+s = [0]*(n+1)
+d[0] = 1
+d[1] = 2
+s[0] = 1
+s[1] = d[0] + d[1]
+for i in range(2, n+1):
+  d[i] = d[i-1] + 2*s[i-2]
+  s[i] = s[i-1] + d[i]
+  d[i] %= 9901
+  s[i] %= 9901
+print(s[n])
