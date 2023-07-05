f111fed21677be8c4da649c81871f6b39a8a2371
김건희 <92205960+kghees@users.noreply.github.com>
2023-07-05 22:54:19 +0900

    [Silver I] Title: 카잉 달력, Time: 3048 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+  m, n, x, y = map(int,input().split())
+  ans = -1
+  while x <= m*n:
+    if (x-y) % n == 0:
+      ans = x
+      break
+    x += m
+  print(ans)
