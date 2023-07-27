d6c7e6df5b5b19c97d6c27e38e970db08b492c30
김건희 <92205960+kghees@users.noreply.github.com>
2023-07-27 14:25:36 +0900

    [D2] Title: Base64 Decoder, Time: 102 ms, Memory: 43,756 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
@@ -1,19 +1,14 @@
-decode = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-      'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-      '0','1','2','3','4','5','6','7','8','9','+','/']
 for t in range(int(input())):
-  s = list(input())
+  d ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+  s = input()
   ans = ''
-  for i in range(len(s)):
-    num = decode.index(s[i])
-    bin_num = str(bin(num)[2:])
-
-    while len(bin_num) < 6:
-      bin_num = '0' + bin_num
-    ans += bin_num
-  
+  arr = []
   result = ''
-  for j in range(len(s)*6 // 8):
-    cnt = int(ans[j*8:j*8+8],2)
-    result += chr(cnt)
+  for i in s:
+    num = d.index(i)
+    ans += bin(num)[2:].zfill(6)
+  for i in range(0, len(ans)-7, 8):
+    arr.append(ans[i:i+8])
+  for i in arr:
+    result += chr(int(i,2))
   print(f'#{t+1} {result}')
