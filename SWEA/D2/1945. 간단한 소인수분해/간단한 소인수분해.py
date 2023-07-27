a85d83ff644a3f8e251467fd3a1adebfb8fa9994
김건희 <92205960+kghees@users.noreply.github.com>
2023-07-27 20:14:05 +0900

    [D2] Title: 간단한 소인수분해, Time: 143 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,16 +1,20 @@
 for t in range(int(input())):
   n = int(input())
-  num = [2,3,5,7,11]
-  arr = [0]*5
-  for i in range(5):
-    while 1:
-      if n % num[i] == 0:
-        arr[i] += 1
-        n = n//num[i]
-      else:
-        break
-
-  print(f'#{t+1}',end=' ')
-  for i in arr:
-    print(i,end=' ')
-  print()
+  a,b,c,d,e = 0,0,0,0,0
+  while n!=1:
+    if n % 2 == 0:
+      a += 1
+      n /= 2
+    elif n % 3 == 0:
+      b += 1
+      n /= 3
+    elif n % 5 == 0:
+      c += 1
+      n /= 5
+    elif n % 7 == 0:
+      d += 1
+      n /= 7
+    elif n % 11 == 0:
+      e += 1
+      n /= 11
+  print(f'#{t+1} {a} {b} {c} {d} {e}')
