fb06ebbd52fbff9775ed44ee7d7da23a8dcc88e3
김건희 <92205960+kghees@users.noreply.github.com>
2023-07-28 17:01:32 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 112 ms, Memory: 43,512 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -6,11 +6,11 @@ for t in range(1, 11):
     for j in range(8-n+1):
       if arr[i][j:j+n] == arr[i][j:j+n][::-1]:
         cnt += 1
-  for i in range(8):
-    for j in range(8-n+1):
+  for i in range(8-n+1):
+    for j in range(8):
       ans = ''
-      for k in range(j,j+n):
-        ans += arr[k][i]
+      for k in range(n):
+        ans += arr[i+k][j]
       if ans == ans[::-1]:
         cnt += 1
   print(f'#{t} {cnt}')
