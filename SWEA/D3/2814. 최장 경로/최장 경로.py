df9f26dd6b97ecbc7366b4dadd36d6d505193c28
김건희 <92205960+kghees@users.noreply.github.com>
2023-07-31 18:25:35 +0900

    [D3] Title: 최장 경로, Time: 152 ms, Memory: 60,596 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,21 +1,22 @@
-def dfs(a,cnt):
+def dfs1(v,cnt):
   global result
-  visited[a] = 1
-  for i in path[a]:
-    if visited[i] == 0:
-      dfs(i, cnt+1)
-  visited[a] = 0 
-  if result < cnt:
-    result = cnt
+  result = max(cnt, result)
+  for k in range(1, n+1):
+    if path[v][k] and not visited[k]:
+      visited[k] = 1
+      dfs1(k,cnt+1)
+      visited[k] = 0
 for t in range(int(input())):
   n,m = map(int,input().split())
-  path = [[] for _ in range(n+1)]
-  visited = [0]*(n+1)
+  path = [[0 for _ in range(n+1)] for _ in range(n+1)]
   for _ in range(m):
     x, y = map(int,input().split())
-    path[x].append(y)
-    path[y].append(x)
-  cnt,result = 0, 0
+    path[x][y] = 1
+    path[y][x] = 1
+
+  result = 1
   for i in range(1, n+1):
-    dfs(i,1)
+    visited = [0]*(n+1)
+    visited[i] = 1
+    dfs1(i,1)
   print(f'#{t+1} {result}')
