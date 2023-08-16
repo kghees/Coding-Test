7555a3aa22b38b1beccd392a071263011cdf8f35
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-16 22:11:14 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 234 ms, Memory: 64,100 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,22 +1,22 @@
-def dfs(index, count):
+def dfs(index,count):
   global result
   if count == int(cnt):
     result = max(int(''.join(num)),result)
     return
-  for i in range(index, len(num)):
-    for j in range(i+1, len(num)):
+  for i in range(index,len(num)):
+    for j in range(i+1,len(num)):
       if num[i] <= num[j]:
-        num[i], num[j] = num[j], num[i]
-        dfs(i, count+1)
-        num[i], num[j] = num[j], num[i]
-  if result == 0 and count < int(cnt):
-    x = (int(cnt) - count) % 2
-    if x == 1:
-      num[-1], num[-2] = num[-2], num[-1]
+        num[i],num[j] = num[j],num[i]
+        dfs(i,count+1)
+        num[i],num[j] = num[j],num[i]
+  if result == 0 and int(cnt) > count:
+    a = (int(cnt) - count) % 2
+    if a == 1:
+      num[-1],num[-2] = num[-2],num[-1]
     dfs(index,int(cnt)) 
-for c in range(int(input())):
+for t in range(int(input())):
   num, cnt = input().split()
   num = list(num)
   result = 0
   dfs(0,0)
-  print(f'#{c+1} {result}')
+  print(f'#{t+1} {result}')
