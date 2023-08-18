abe4caa3b901ee82519926c7e565582090e2e9b5
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-18 22:19:27 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 106 ms, Memory: 43,520 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,10 +1,10 @@
-def squre(n,m):
+def square(n,m):
   if m > 1:
-    return n*squre(n,m-1)
+    return n*square(n,m-1)
   else:
     return n
-for _ in range(1, 11):
-  t = int(input())
+for t in range(10):
+  n = int(input())
   n, m = map(int,input().split())
-  result = squre(n,m)
-  print(f'#{t} {result}')
+  result = square(n,m)
+  print(f'#{t+1} {result}')
