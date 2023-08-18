daea28e92895d270e51e823701cf36e333dfad44
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-18 16:05:47 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 176 ms, Memory: 60,540 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,26 +1,24 @@
 for t in range(10):
-  c = int(input())
-  arr = [list(map(int,input().split()))for _ in range(100)]
-  a = 0
+  n = int(input())
+  arr = [list(map(int,input().split())) for _ in range(100)]
+  x = 0
   for i in range(100):
-    cnt = 0
+    a = sum(arr[i])
+    if a > x:
+      x = a
+  y = 0
+  for i in range(100):
+    b = 0
     for j in range(100):
-      cnt += arr[i][j]
-    if cnt > a:
-      a = cnt
-  b = 0
+      b += arr[j][i]
+    if b > y:
+      y = b
+  z = 0
+  c, d = 0, 0
   for i in range(100):
-    cnt = 0
-    for j in range(100):
-      cnt += arr[j][i]
-    if cnt > b:
-      b = cnt
-  c = 0
-  for i in range(100):
-    cnt, cnt1 = 0, 0
-    cnt += arr[i][i]
-    cnt1 += arr[i][99-i]
-    if max(cnt,cnt1) > c:
-      c = max(cnt,cnt1)
-  x = max(a,b,c)
-  print(f'#{t+1} {x}')
+    c += arr[i][i]
+    d += arr[i][99-i]
+    if max(c,d) > z:
+      z = max(c,d)
+  w = max(x,y,z)
+  print(f'#{t+1} {w}')
