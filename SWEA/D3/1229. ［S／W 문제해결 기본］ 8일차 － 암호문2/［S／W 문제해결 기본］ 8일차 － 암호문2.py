4fcea456170ad28703e33f912c97cfbf88ffc705
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-19 16:35:38 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 106 ms, Memory: 50,932 KB -BaekjoonHub

--- SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.py
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.py
@@ -1,19 +1,19 @@
-for t in range(1,11):
+for t in range(10):
   n = int(input())
   arr = list(map(int,input().split()))
   m = int(input())
   brr = list(input().split())
   for i in range(len(brr)):
     if brr[i] == 'I':
-      start = int(brr[i+1])
-      end = int(brr[i+2])
-      num = list(brr[i+3:end+i+3])
-      arr[start:start] = num
+      a = int(brr[i+1])
+      b = int(brr[i+2])
+      num = list(map(int,brr[i+3:b+i+3]))
+      arr[a:a] = num
     elif brr[i] == 'D':
-      start = int(brr[i+1])
-      end = int(brr[i+2])
-      del arr[start:start+end]
-  print(f'#{t}',end=' ')
+      a = int(brr[i+1])
+      b = int(brr[i+2])
+      del arr[a:a+b]
+  print(f'#{t+1}',end=' ')
   for i in arr[:10]:
     print(i,end=' ')
   print()
