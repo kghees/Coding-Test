e4ed16ccee3ee9dcec7027432b31919500942ff8
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-23 23:03:15 +0900

    [D3] Title: 부분 수열의 합, Time: 3,989 ms, Memory: 74,904 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,12 +1,15 @@
+def part_sum(x):
+  global cnt
+  if sum(ans) == k and len(ans) > 0:
+    cnt += 1
+  for i in range(x, n):
+    ans.append(a[i])
+    part_sum(i+1)
+    ans.pop()
 for t in range(int(input())):
   n, k = map(int,input().split())
   a = list(map(int,input().split()))
   cnt = 0
-  for i in range(1, (1<<n)):
-    s = 0
-    for j in range(n):
-      if (i & (1<<j)) > 0:
-        s += a[j]
-    if k == s:
-      cnt += 1
+  ans = []
+  part_sum(0)
   print(f'#{t+1} {cnt}')
