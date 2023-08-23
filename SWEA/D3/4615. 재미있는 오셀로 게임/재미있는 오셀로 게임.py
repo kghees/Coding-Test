18429795bdfdcf2478bdd34b1b8d13d29f42ca0a
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-23 13:21:00 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 259 ms, Memory: 63,580 KB -BaekjoonHub

--- SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
@@ -1,23 +1,22 @@
-#4615 재미있는 오셀로 게임
-d = [(0,1),(1,0),(-1,0),(0,-1),(1,1),(-1,1),(1,-1),(-1,-1)]
+d = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
 for tc in range(int(input())):
   n, m = map(int,input().split())
   arr = [[0]*n for _ in range(n)]
   mid = n // 2
   arr[mid][mid] = 2
   arr[mid-1][mid-1] = 2
-  arr[mid-1][mid] = 1
   arr[mid][mid-1] = 1
-  for i in range(m):
+  arr[mid-1][mid] = 1
+  for _ in range(m):
     x,y,t = map(int,input().split())
-    x, y = x-1,y-1
+    x, y = x - 1, y - 1
     data = []
-    for j in range(8):
-      dx,dy = d[j]
-      nx = x + dx
-      ny = y + dy
+    for i in range(8):
+      dx,dy = d[i]
+      nx = dx + x
+      ny = dy + y
       while True:
-        if not (0<= nx < n and 0 <= ny < n):
+        if not (0<=nx<n and 0<=ny<n):
           data = []
           break
         if arr[nx][ny] == 0:
@@ -33,9 +32,9 @@ for tc in range(int(input())):
           arr[tx][ty] = 1
         elif t == 2:
           arr[tx][ty] = 2
-    arr[x][y] = t
-  w,b = 0,0
-  for i in range(n):
-    w += arr[i].count(2)
-    b += arr[i].count(1)
-  print(f'#{tc+1} {b} {w}')
+      arr[x][y] = t
+    w, b = 0,0
+    for i in range(n):
+      w += arr[i].count(2)
+      b += arr[i].count(1)
+  print(f'#{tc+1} {b} {w}')    
