398a256fb9418aa433a2914674eebd68da1f1d97
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-24 13:08:22 +0900

    [D3] Title: 오목 판정, Time: 210 ms, Memory: 62,768 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -5,7 +5,7 @@ def omok():
     for j in range(n):
       if arr[i][j] == 'o':
         for k in range(4):
-          x, y = i, j
+          x, y = i,j
           cnt = 0
           while 0 <= x < n and 0 <= y < n and arr[x][y] == 'o':
             cnt += 1
@@ -16,5 +16,5 @@ def omok():
   return 'NO'
 for t in range(int(input())):
   n = int(input())
-  arr = [input() for _ in range(n)]
+  arr = [list(input()) for _ in range(n)]
   print(f'#{t+1} {omok()}')
