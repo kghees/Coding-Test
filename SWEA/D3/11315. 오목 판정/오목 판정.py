80599d24fbabe1a1f0b9e890ef965b157a1377dd
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-24 12:48:11 +0900

    [D3] Title: 오목 판정, Time: 236 ms, Memory: 62,768 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,22 +1,20 @@
 def omok():
-  dx = [0,1,1,1]
-  dy = [1,0,1,-1]
+  dx = [1,0,1,1]
+  dy = [-1,1,0,1]
   for i in range(n):
     for j in range(n):
-      if s[i][j] == 'o':
+      if arr[i][j] == 'o':
         for k in range(4):
-          x = i
-          y = j
+          x, y = i, j
           cnt = 0
-          while 0 <= x < n and 0 <= y < n and s[x][y] == 'o':
+          while 0 <= x < n and 0 <= y < n and arr[x][y] == 'o':
             cnt += 1
             x += dx[k]
             y += dy[k]
           if cnt >= 5:
             return 'YES'
   return 'NO'
-          
 for t in range(int(input())):
   n = int(input())
-  s = [input() for _ in range(n)]
+  arr = [input() for _ in range(n)]
   print(f'#{t+1} {omok()}')
