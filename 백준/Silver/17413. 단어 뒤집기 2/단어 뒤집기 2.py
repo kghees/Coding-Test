851d71f6d91797a434fc7d8ca4e935a798b282dc
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-25 21:15:20 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 84 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,27 +1,23 @@
-import sys
-input = sys.stdin.readline
-s = input().rstrip()
-tag = False
-result = ''
+s = input()
 stack = []
-for i in s:
-  if i == ' ':
-    for _ in range(len(stack)):
-      result += stack.pop()
-    result += i
-  elif i == '<':
-    tag = True
-    for _ in range(len(stack)):
-      result += stack.pop()
-    result += i
-  elif i == '>':
-    tag = False
-    result += i
-  elif tag:
-    result += i
+result = []
+for i in range(len(s)):
+  if s[i] == '>':
+    stack.append('>')
+    result.append(''.join(stack))
+    stack = []
+  elif s[i] == '<' and stack:
+    stack.reverse()
+    result.append(''.join(stack))
+    stack = [s[i]]
+  elif s[i] == ' ' and '<' not in stack:
+    stack.reverse()
+    result.append(''.join(stack))
+    result.append(' ')
+    stack = []
   else:
-    stack.append(i)
-
-for _ in range(len(stack)):
-  result += stack.pop()
-print(result)
+    stack.append(s[i])
+if stack:
+  stack.reverse()
+  result.append(''.join(stack))
+print(''.join(result))
