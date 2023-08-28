5d74cd2dae769d9cf54d4a3e31b036430e555f08
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-28 21:50:02 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -1,19 +1,20 @@
 n = int(input())
-s = list(map(int,input().split()))
+arr = list(map(int,input().split()))
 stack = []
 i = 1
-while s:
-  if s[0] == i:
-    s.pop(0)
-    i += 1
-  else:
-    stack.append(s.pop(0))
-  while stack:
-    if stack[-1] == i:
-      stack.pop()
+for _ in range(n):
+  while arr:
+    if arr[0] == i:
+      arr.pop(0)
       i += 1
     else:
-      break
+      stack.append(arr.pop(0))
+    while stack:
+      if stack[-1] == i:
+        stack.pop()
+        i += 1
+      else:
+        break
 if not stack:
   print('Nice')
 else:
