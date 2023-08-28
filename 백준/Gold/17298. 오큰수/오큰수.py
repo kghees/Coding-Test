b512cffc66767782d6e6a732428e7ec417a2b118
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-28 22:44:32 +0900

    [Gold IV] Title: 오큰수, Time: 660 ms, Memory: 253236 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,12 +1,14 @@
-import sys
-input = sys.stdin.readline
 n = int(input())
-a = list(map(int, input().split()))
-stack = []
-result = [-1] * n
-stack.append(0)
+a = list(map(int,input().split()))
+result = [0]*n
+stack = [0]
 for i in range(1, n):
-   while stack and a[stack[-1]] < a[i]:
+  if not stack:
+    stack.append(i)
+  while stack and a[stack[-1]] < a[i]:
     result[stack.pop()] = a[i]
-   stack.append(i)
-print(*result)
+  stack.append(i)
+while stack:
+  result[stack.pop()] = -1
+for i in result:
+  print(i,end=' ')
