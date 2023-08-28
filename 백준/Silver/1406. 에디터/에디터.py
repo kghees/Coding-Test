2f06ac915da9935e402827e1e880be3f9ab80c59
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-28 13:51:43 +0900

    [Silver II] Title: 에디터, Time: 464 ms, Memory: 179508 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,15 +1,15 @@
 left = list(input())
-right = []
 m = int(input())
+right = []
 for _ in range(m):
-  arr = list(input().split())
-  if left and arr[0] == 'L':
+  s = list(input().split())
+  if left and s[0] == 'L':
     right.append(left.pop())
-  elif right and arr[0] == 'D':
+  elif right and s[0] == 'D':
     left.append(right.pop())
-  elif left and arr[0] == 'B':
+  elif left and s[0] == 'B':
     left.pop()
-  elif arr[0] == 'P':
-    left.append(arr[1])
+  elif s[0] == 'P':
+    left.append(s[1])
 right.reverse()
 print(''.join(left+right))
