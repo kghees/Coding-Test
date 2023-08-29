7a4ef63544deb563bb9e063f5e1c4c5326cf960e
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-29 21:32:51 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 108 ms, Memory: 42172 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.py
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.py
@@ -1,13 +1,12 @@
-import sys
-import math
-input = sys.stdin.readline
-n, s = map(int,input().split())
-arr = list(map(int,input().split()))
-for i in range(n):
-  if arr[i] - s < 0:
-    arr[i] = abs(arr[i]-s)
+def gcd(x, y):
+  if y == 0:
+    return x
   else:
-    arr[i] = arr[i] - s
-for i in range(1,n):
-  arr[0] = math.gcd(arr[0],arr[i])
-print(arr[0])
+    return gcd(y, x%y)
+n, s = map(int,input().split())
+a = list(map(int,input().split()))
+a = [abs(i-s) for i in a]
+ans = a[0]
+for i in range(1, n):
+  ans = gcd(ans,a[i])
+print(ans)
