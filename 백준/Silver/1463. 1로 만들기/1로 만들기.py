acb761cd2e413eca006baf103cf0bbf9fe73d3a8
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-30 13:09:54 +0900

    [Silver III] Title: 1로 만들기, Time: 452 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,11 +1,10 @@
-import sys
-input = sys.stdin.readline
-x = int(input())
-d = [0] *(x+1)
-for i in range(2, x+1):
+n = int(input())
+d = [0]*(n+1)
+d[1] = 0
+for i in range(2, n+1):
   d[i] = d[i-1] + 1
-  if i % 2 == 0 and d[i] > d[i//2] + 1:
+  if i % 2 == 0 and  d[i] > d[i//2] + 1:
     d[i] = d[i//2] + 1
   if i % 3 == 0 and d[i] > d[i//3] + 1:
     d[i] = d[i//3] + 1
-print(d[x])
+print(d[n])
