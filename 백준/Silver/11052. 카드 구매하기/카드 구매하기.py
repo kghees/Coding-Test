edaac2700e3df32b9a39e5d19bf94afccf7ef83e
김건희 <92205960+kghees@users.noreply.github.com>
2023-08-30 21:56:13 +0900

    [Silver I] Title: 카드 구매하기, Time: 220 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -3,5 +3,5 @@ p = [0] + list(map(int,input().split()))
 d = [0]*(n+1)
 for i in range(1, n+1):
   for j in range(1, i+1):
-    d[i] = max(d[i],d[i-j]+p[j])
-print(d[i])
+    d[i] = max(d[i], d[i-j] + p[j])
+print(d[n])
