eabff4e0f90658f08831b23a6182713885ffaa98
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-01 20:29:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 5672 ms, Memory: 229220 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,17 +1,14 @@
-import sys
-input = sys.stdin.readline
 n, k = map(int,input().split())
 arr = [[0,0]]
 for _ in range(n):
   arr.append(list(map(int,input().split())))
-knapsack = [[0 for _ in range(k+1)] for _ in range(n+1)]
+d = [[0 for _ in range(k+1)] for _ in range(n+1)]
 for i in range(1, n+1):
   for j in range(1, k+1):
     weight = arr[i][0]
     value = arr[i][1]
-    
     if j < weight:
-      knapsack[i][j] = knapsack[i-1][j]
+      d[i][j] = d[i-1][j]
     else:
-      knapsack[i][j] = max(value + knapsack[i-1][j-weight], knapsack[i-1][j])
-print(knapsack[n][k])
+      d[i][j] = max(d[i-1][j] ,d[i-1][j-weight]+value)
+print(d[n][k])
