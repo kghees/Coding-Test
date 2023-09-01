caad66f2090f0001fb159eafa32a109befe86074
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-01 16:59:07 +0900

    [Gold IV] Title: RGB거리 2, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -1,22 +1,14 @@
 n = int(input())
-ans = 1000*1000+1
-a = [[0]*n for _ in range(n+1)]
-for i in range(1,n+1):
-  a[i][0], a[i][1], a[i][2] = map(int,input().split())
-d = [[0]*n for _ in range(n+1)]
-for k in range(3):
-  for j in range(3):
-    if j == k:
-      d[1][j] = a[1][j]
-    else:
-      d[1][j] = ans
-  for i in range(2, n+1):
-    d[i][0] = min(d[i-1][1], d[i-1][2]) + a[i][0]
-    d[i][1] = min(d[i-1][0], d[i-1][2]) + a[i][1]
-    d[i][2] = min(d[i-1][0], d[i-1][1]) + a[i][2]
-  for j in range(3):
-    if j == k:
-      continue
-    else:
-      ans = min(ans,d[n][j])
+rgb = [list(map(int,input().split())) for _ in range(n)]
+ans = 1000*1000 + 1
+for i in range(3):
+  d = [[ans,ans,ans] for _ in range(n)]
+  d[0][i] = rgb[0][i]
+  for j in range(1, n):
+    d[j][0] = min(d[j-1][1], d[j-1][2]) + rgb[j][0]
+    d[j][1] = min(d[j-1][0],d[j-1][2]) + rgb[j][1]
+    d[j][2] = min(d[j-1][0], d[j-1][1]) + rgb[j][2]
+  for k in range(3):
+    if i != k:
+      ans = min(ans,d[-1][k])
 print(ans)
