591eac38ca35dd1a014a15817ff9a07b82cd5db7
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-04 21:16:22 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,15 +1,14 @@
-a = []
-for _ in range(9):
-  a.append(int(input()))
-result = sum(a)
+n = 9
+a = [int(input()) for _ in range(n)]
 a.sort()
-for i in range(len(a)):
-  for j in range(i+1,len(a)):
-    if result - a[i] - a[j] == 100:
-      for k in range(len(a)):
-        if i == k or j == k:
-          continue
-        else:
-          print(a[k])
-      exit()
-      
+t1,t2 = 0, 0
+ans = sum(a)
+for i in range(n):
+  for j in range(i+1,n):
+    if ans - (a[i] + a[j]) == 100:
+      t1 = a[i]
+      t2 = a[j]
+a.remove(t1)
+a.remove(t2)
+for i in a:
+  print(i)
