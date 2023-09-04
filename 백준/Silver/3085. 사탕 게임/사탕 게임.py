8df23e170801e22d67d1e2c3f09fdccbffdfc04e
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-04 22:38:42 +0900

    [Silver II] Title: 사탕 게임, Time: 1912 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,41 +1,39 @@
-import sys
-input = sys.stdin.readline
 def check(a):
-  n = len(a)
-  ans = 1
-  for i in range(n):
+  x = len(a)
+  t = 1
+  for i in range(x):
     cnt = 1
-    for j in range(1, n):
+    for j in range(1, x):
       if a[i][j] == a[i][j-1]:
         cnt += 1
       else:
         cnt = 1
-      if cnt > ans:
-        ans = cnt
+      if t < cnt:
+        t = cnt
     cnt = 1
-    for j in range(1, n):
+    for j in range(1, x):
       if a[j][i] == a[j-1][i]:
         cnt += 1
       else:
         cnt = 1
-      if cnt > ans:
-        ans = cnt
-  return ans     
+      if t < cnt:
+        t = cnt
+  return t
 n = int(input())
 a = [list(input()) for _ in range(n)]
 ans = 0
 for i in range(n):
   for j in range(n):
     if j+1 < n:
-      a[i][j], a[i][j+1] = a[i][j+1], a[i][j]
+      a[i][j],a[i][j+1] = a[i][j+1],a[i][j]
       temp = check(a)
-      if temp > ans:
+      if ans < temp:
         ans = temp
-      a[i][j], a[i][j+1] = a[i][j+1], a[i][j]
+      a[i][j],a[i][j+1] = a[i][j+1],a[i][j]
     if i+1 < n:
-      a[i][j], a[i+1][j] = a[i+1][j], a[i][j]
+      a[i][j],a[i+1][j] = a[i+1][j],a[i][j]
       temp = check(a)
-      if temp > ans:
+      if ans < temp:
         ans = temp
-      a[i][j], a[i+1][j] = a[i+1][j], a[i][j]
+      a[i][j],a[i+1][j] = a[i+1][j],a[i][j]
 print(ans)
