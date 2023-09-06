da361d879462c3fe6f5dab4fd6140f868ce09041
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-06 12:24:30 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,7 +1,7 @@
-n,m = map(int,input().split())
-a = [0]*m
+n, m = map(int,input().split())
 c = [False]*(n+1)
-def go(index,n, m):
+a = [0]*m
+def go(index,n,m):
   if index == m:
     print(*a)
     return
@@ -10,6 +10,6 @@ def go(index,n, m):
       continue
     c[i] = True
     a[index] = i
-    go(index+1, n, m)
+    go(index+1,n,m)
     c[i] = False
 go(0,n,m)
