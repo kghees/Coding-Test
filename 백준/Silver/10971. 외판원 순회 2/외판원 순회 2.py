d5e64c2163274698c3df8aad65f42a16b4ad3977
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-07 18:41:35 +0900

    [Silver II] Title: 외판원 순회 2, Time: 176 ms, Memory: 115380 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,13 +1,13 @@
 def next_permutation(a):
   i = len(a) - 1
-  while i > 0 and a[i-1] >= a[i]:
+  while i > 0 and a[i] <= a[i-1]:
     i -= 1
   if i <= 0:
     return False
   j = len(a) - 1
   while a[j] <= a[i-1]:
     j -= 1
-  a[i-1],a[j] = a[j], a[i-1]  
+  a[i-1],a[j] = a[j],a[i-1]
   j = len(a) - 1
   while i < j:
     a[i],a[j] = a[j],a[i]
@@ -15,22 +15,23 @@ def next_permutation(a):
     j -= 1
   return True
 n = int(input())
-w = [list(map(int,input().split())) for _ in range(n)]
+a = [list(map(int,input().split())) for _ in range(n)]
 d = [i for i in range(n)]
 ans = 2147483647
 while True:
   ok = True
-  s = 0
+  num = 0
   for i in range(0, n-1):
-    if w[d[i]][d[i+1]] == 0:
+    if a[d[i]][d[i+1]] == 0:
       ok = False
       break
     else:
-      s += w[d[i]][d[i+1]]
-  if ok and w[d[n-1]][d[0]] != 0:
-    s += w[d[n-1]][d[0]]
-    if ans > s:
-      ans = s
+      num += a[d[i]][d[i+1]]
+  if ok and a[d[-1]][d[0]] != 0:
+    num += a[d[-1]][d[0]]
+    ans = min(ans,num)
   if not next_permutation(d):
+    break
+  if d[0] != 0:
     break
 print(ans)
