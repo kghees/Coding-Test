0d26f0e33704cb93ca3c22994a65647490101a3d
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-07 14:36:26 +0900

    [Silver III] Title: 다음 순열, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.py
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -1,18 +1,16 @@
-import sys
-input = sys.stdin.readline
 def next_permutation(a):
-  i = len(a) - 1
+  i = len(a)-1
   while i > 0 and a[i-1] >= a[i]:
     i -= 1
   if i <= 0:
     return False
-  j = len(a) - 1
+  j = len(a)-1
   while a[j] <= a[i-1]:
     j -= 1
-  a[i-1],a[j] = a[j], a[i-1]
-  j = len(a) - 1
+  a[i-1],a[j] = a[j],a[i-1]
+  j = len(a)-1
   while i < j:
-    a[i], a[j] = a[j], a[i]
+    a[i],a[j] = a[j],a[i]
     i += 1
     j -= 1
   return True
