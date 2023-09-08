286ddf7bc31208b0e2a10844628f352318db557f
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-08 14:57:53 +0900

    [Silver I] Title: 부등호, Time: 180 ms, Memory: 117364 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -6,23 +6,22 @@ def good(x,y,op):
     if x < y:
       return False
   return True
-  
 def go(index, num):
-  if index == n+1:
+  if index == k+1:
     ans.append(num)
     return
   for i in range(10):
     if check[i]:
       continue
-    if (index == 0 or good(num[index-1], str(i), a[index-1])):
+    if index == 0 or good(num[index-1],str(i),s[index-1]):
       check[i] = True
-      go(index+1, num+str(i))
+      go(index+1,num+str(i))
       check[i] = False
-n = int(input())
-a = input().split()
+k = int(input())
+s = input().split()
 ans = []
 check = [False]*10
-go(0, '')
+go(0,'')
 ans.sort()
 print(ans[-1])
 print(ans[0])
