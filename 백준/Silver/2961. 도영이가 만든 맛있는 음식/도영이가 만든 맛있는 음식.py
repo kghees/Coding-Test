c6901dba192bba70313d1f4308be1a75a80a013b
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-10 16:11:05 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,23 +1,14 @@
-def food(x,start):
-  global result
-  if x == a:
-   l = 1
-   b = 0
-   for i in arr:
-    l *= i[0]
-    b += i[1]
-   if abs(l-b) < result:
-    result = abs(l-b)
-   return
-  for i in range(start,n):
-    arr.append(s[i])
-    food(x+1,i+1)
-    arr.pop()
+def food(x,y,l,b):
+  global cnt
+  if y != 0:
+    if abs(l-b) < cnt:
+      cnt = abs(l-b)
+  if x == n:
+    return
+  food(x+1,y+1,l*s[x][0],b+s[x][1])
+  food(x+1,y,l,b)
 n = int(input())
 s = [list(map(int,input().split())) for _ in range(n)]
-result = 1000000001
-arr = []
-for i in range(1, n+1):
-  a = i
-  food(0,0)
-print(result)
+cnt = 1000000001
+food(0,0,1,0)
+print(cnt)
