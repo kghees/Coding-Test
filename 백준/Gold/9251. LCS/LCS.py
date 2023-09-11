a620f4c5edcbfd383f1141e67ccaee275fbec55a
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-11 22:08:37 +0900

    [Gold V] Title: LCS, Time: 524 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -8,5 +8,5 @@ for i in range(1, x+1):
     if a[i-1] == b[j-1]:
       d[i][j] = d[i-1][j-1] + 1
     else:
-      d[i][j] = max(d[i-1][j], d[i][j-1])
-print(d[-1][-1]) 
+      d[i][j] = max(d[i-1][j],d[i][j-1])
+print(d[-1][-1])
