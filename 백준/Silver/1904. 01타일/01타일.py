482174f606cc4b7d558aa767b4b561e99c6cc162
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-11 21:51:02 +0900

    [Silver III] Title: 01타일, Time: 324 ms, Memory: 69788 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,9 +1,7 @@
-import sys
-input = sys.stdin.readline
 n = int(input())
-d = [0]*(1000001)
+d = [0]*1000001
 d[1] = 1
 d[2] = 2
-for i in range(3, n+1):
-  d[i] = (d[i-1] + d[i-2]) % 15746
-print(d[n])
+for i in range(3, 1000001):
+  d[i] = (d[i-1]+d[i-2]) % 15746
+print(d[n] % 15746)
