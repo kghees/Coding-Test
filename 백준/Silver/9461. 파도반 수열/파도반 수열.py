c38c4dede508415a893d55f36336c1d24868a1e1
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-11 17:54:14 +0900

    [Silver III] Title: 파도반 수열, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,8 +1,7 @@
-t = int(input())
-d = [0 for i in range(101)]
-d[1],d[2],d[3] = 1, 1, 1
-for i in range(4, 101):
-  d[i] = d[i-3] + d[i-2]
-for _ in range(t):
+for t in range(int(input())):
   n = int(input())
-  print(d[n])
+  d = [0]*101
+  d[0],d[1],d[2] = 1,1,1
+  for i in range(3, 101):
+    d[i] = d[i-2]+d[i-3]
+  print(d[n-1])
