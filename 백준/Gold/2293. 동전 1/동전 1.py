5a49b88199baa6aff810bb876374656be175c45a
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-12 11:47:09 +0900

    [Gold V] Title: 동전 1, Time: 180 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,8 +1,8 @@
 n, k = map(int,input().split())
 a = [int(input()) for _ in range(n)]
-d = [0 for _ in range(k+1)]
+d = [0]*(k+1)
 d[0] = 1
 for i in a:
-  for j in range(i, k+1):
+  for j in range(i,k+1):
     d[j] += d[j-i]
 print(d[k])
