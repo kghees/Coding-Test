1ff24a842290a3e3ebc381c78051f3a3e8a6414f
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-12 22:25:01 +0900

    [Gold V] Title: 합분해, Time: 56 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,7 +1,9 @@
 n, k = map(int,input().split())
-d = [[0]*(k+1) for i in range(n+1)]
-d[0][0] = 1
-for i in range(n+1):
-  for j in range(1, k+1):
-    d[i][j] = d[i-1][j] + d[i][j-1]
-print(d[n][k]% 1000000000)
+d = [[0]*(n+1) for _ in range(k+1)]
+for i in range(1, n+1):
+  d[1][i] = 1
+for i in range(2, k+1):
+  d[i][1] = i
+  for j in range(2, n+1):
+    d[i][j] = (d[i-1][j] + d[i][j-1]) % 1000000000
+print(d[k][n])
