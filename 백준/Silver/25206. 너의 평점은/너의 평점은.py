4e11318e86b50c537956e85705126b369225982a
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-13 14:12:27 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,11 @@
+score = [4.5,4.0,3.5,3.0,2.5,2.0,1.5,1.0,0.0]
+grade = ['A+','A0','B+','B0','C+','C0','D+','D0','F']
+total = 0
+result = 0
+for _ in range(20):
+  n,s,g = input().split()
+  s = float(s)
+  if g != 'P':
+    total += s
+    result += s*score[grade.index(g)]
+print('%.6f' % (result / total))
