ccb80c67fa95592e763202c3b5ace168fc651a17
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-13 23:32:31 +0900

    [Silver V] Title: 영화감독 숌, Time: 632 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+cnt = 0
+num = 1
+while True:
+  if '666' in str(num):
+    cnt += 1
+  if cnt == n:
+    print(num)
+    break
+  num += 1
