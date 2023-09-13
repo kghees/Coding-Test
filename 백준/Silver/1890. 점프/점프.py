740d33660a2ff1dc6f9e661d5250ec9948ab2058
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-13 21:15:12 +0900

    [Silver I] Title: 점프, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -1,8 +1,6 @@
-import sys
-input = sys.stdin.readline
 n = int(input())
 a = [list(map(int,input().split())) for _ in range(n)]
-d = [[0]*n for _ in range(n)]
+d = [[0]*(n) for _ in range(n)]
 d[0][0] = 1
 for i in range(n):
   for j in range(n):
