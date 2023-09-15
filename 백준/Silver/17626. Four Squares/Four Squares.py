51bf7d6ea35889e400c799de2deb2b9161a730cf
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-15 22:20:43 +0900

    [Silver III] Title: Four Squares, Time: 144 ms, Memory: 116904 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,20 +1,10 @@
 n = int(input())
-arr = []
-for i in range(n+1):
-  if i**0.5 % 1:
-    arr.append(0)
-  else:
-    arr.append(1)
-m = 4
-for i in range(int(n**0.5),0,-1):
-  if arr[n]:
-    m = 1
-    break
-  elif arr[n-i**2]:
-    m = 2
-    break
-  else:
-    for j in range(int((n-i**2)**0.5),0,-1):
-      if arr[(n-i**2)-j**2]:
-        m = 3
-print(m)
+d = [0,1]
+for i in range(2, n+1):
+  x = 4
+  j = 1
+  while j*j <= i:
+    x = min(x,d[i-j*j])
+    j += 1
+  d.append(x+1)
+print(d[n])
