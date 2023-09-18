181d3cfb7a21719ad30ddd716154813fafc7f4e2
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-18 16:34:32 +0900

    [Silver V] Title: 폴리오미노, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,17 +1,7 @@
 a = input()
-cnt = 0
-ans = ''
-while cnt < len(a):
-  if a[cnt:cnt+4] == 'XXXX':
-    ans += 'AAAA'
-    cnt += 4
-  elif a[cnt:cnt+2] == 'XX':
-    ans += 'BB'
-    cnt += 2
-  elif a[cnt] == 'X':
-    ans = -1
-    break
-  else:
-    ans += a[cnt]
-    cnt += 1
-print(ans)
+a = a.replace('XXXX', 'AAAA')
+a = a.replace('XX','BB')
+if 'X' in a:
+  print(-1)
+else:
+  print(a)
