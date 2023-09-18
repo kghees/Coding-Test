2a8e9c4388e1974a0afd3f83b56f00ecb072567e
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-18 17:30:42 +0900

    [Silver IV] Title: 반복수열, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.py
+++ 백준/Silver/2331. 반복수열/반복수열.py
@@ -1,17 +1,10 @@
 a, p = map(int,input().split())
-arr = []
-arr.append(a)
-index = 0
+num = [a]
 while True:
-  result = 0
-  for i in str(arr[-1]):
-    result += int(i)**p
-  if result in arr:
-    for j in range(len(arr)):
-      if arr[j] == result:
-        index = j
-        break
+  ans = 0
+  for i in str(num[-1]):
+    ans += int(i)**p
+  if ans in num:
     break
-  arr.append(result)
-del arr[index:]
-print(len(arr))
+  num.append(ans)
+print(num.index(ans))
