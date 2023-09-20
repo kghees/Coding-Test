a2ed337c10dfb697831ce27808d82f4fa4ddab24
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-20 23:27:17 +0900

    [Gold IV] Title: 이분 그래프, Time: 1260 ms, Memory: 53344 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 k = int(input())
 for _ in range(k):
@@ -10,18 +10,19 @@ for _ in range(k):
     u,v = map(int,input().split())
     a[u].append(v)
     a[v].append(u)
-  def dfs(x, c):
+  def dfs(x,c):
     color[x] = c
     for i in a[x]:
-      if not color[i]:
-        dfs(i,3-c)
+      if color[i] == 0:
+        if not dfs(i,3-c):
+          return False
+      elif color[i] == color[x]:
+        return False
+    return True
   ans = True
-  for i in range(n):
-    if not color[i]:
-      dfs(i,1)
-  for i in range(n):
-    for j in a[i]:
-      if color[i] == color[j]:
+  for i in range(1,n+1):
+    if color[i] == 0:
+      if not dfs(i,1):
         ans = False
   if ans:
     print('YES')
