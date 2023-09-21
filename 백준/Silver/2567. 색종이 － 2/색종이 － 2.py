a7892adf70a00eb4515cd8c267e22abc24989e60
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-21 15:56:53 +0900

    [Silver IV] Title: 색종이 - 2, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,16 +1,22 @@
 t = int(input())
-d = [[0]*101 for _ in range(101)]
+a = [[0]*101 for _ in range(101)]
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 for _ in range(t):
   x,y = map(int,input().split())
-  for i in range(10):
-    for j in range(10):
-      d[x+i][y+j] = 1
-  result = 0
-  for i in range(101):
-    for j in range(101):
+  for i in range(x,x+10):
+    for j in range(y,y+10):
+      a[i][j] = 1
+result = 0
+for i in range(1,101):
+  for j in range(1,101):
+    if a[i][j] == 1:
+      cnt = 0
       for k in range(4):
-        if d[i][j] == 1 and d[i+dx[k]][j+dy[k]] == 0:
-          result += 1
+        if a[i+dx[k]][j+dy[k]] == 1:
+          cnt += 1
+      if cnt == 3:
+        result += 1
+      elif cnt == 2:
+        result += 2
 print(result)
