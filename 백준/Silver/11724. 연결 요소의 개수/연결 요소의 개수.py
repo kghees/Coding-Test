7fd46127964b0ed1c5b2a7d1774cb1acab35e4b0
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-21 21:18:00 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 676 ms, Memory: 251552 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -2,15 +2,15 @@ import sys
 sys.setrecursionlimit(100000)
 def dfs(x):
   check[x] = True
-  for i in a[x]:
-    if not check[i]:
+  for i in range(1, n+1):
+    if not check[i] and a[x][i]:
       dfs(i)
 n,m = map(int,input().split())
-a = [[] for _ in range(n+1)]
+a = [[False]*(n+1) for _ in range(n+1)]
 for _ in range(m):
   u,v = map(int,input().split())
-  a[u].append(v)
-  a[v].append(u)
+  a[u][v] = True
+  a[v][u] = True
 check = [False]*(n+1)
 cnt = 0
 for i in range(1, n+1):
