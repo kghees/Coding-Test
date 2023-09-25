33e7f6b74afd2e1d95dad88cf2a2b768e531fe19
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-25 15:56:07 +0900

    [D2] Title: 달팽이 숫자, Time: 164 ms, Memory: 58,800 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,22 +1,22 @@
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
-def dfs(idx,num,x,y):
+def snail(num,x,y):
   if num > n*n:
     return
-  nx = x + dx[idx]
-  ny = y + dy[idx]
-  if 0 <= nx < n and 0 <= ny < n and d[nx][ny] == 0:
-    d[nx][ny] = num
-    dfs(idx,num+1,nx,ny)
-  else:
-    dfs((idx+1)%4,num,x,y)
+  for k in range(4):
+    nx = x + dx[k]
+    ny = y + dy[k]
+    if 0 <= nx < n and 0 <= ny < n and d[nx][ny] == 0:
+      d[nx][ny] = num
+      snail(num+1,nx,ny)
+  return
 for t in range(int(input())):
   n = int(input())
   d = [[0]*n for _ in range(n)]
   d[0][0] = 1
-  dfs(0,2,0,0)
+  snail(2,0,0)
   print(f'#{t+1}')
   for i in range(n):
     for j in range(n):
-      print(f'{d[i][j]}',end=' ')
+      print(d[i][j], end=' ')
     print()
