a1e548c3e1621a97ff61f6ec8e84faf74ca51dfd
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-26 10:58:00 +0900

    [D2] Title: 패턴 마디의 길이, Time: 149 ms, Memory: 58,240 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.py
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.py
@@ -1,6 +1,6 @@
 for t in range(int(input())):
-  s = input()
-  for i in range(1,10):
-    if s[:i] == s[i:2*i]:
+  a = input()
+  for i in range(1, 10):
+    if a[:i] == a[i:2*i]:
       print(f'#{t+1} {i}')
       break
