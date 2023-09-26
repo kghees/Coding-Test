494056b2cffe87a750000fcc811731be939363e0
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-26 10:32:17 +0900

    [D2] Title: 스도쿠 검증, Time: 150 ms, Memory: 58,260 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,26 +1,23 @@
-for t in range(int(input())):
-  arr = [list(map(int,input().split())) for _ in range(9)]
-  s = []
+def sdoku(a):
   for i in range(9):
-    cnt = [0]*10
-    for k in range(9):
-      cnt[arr[i][k]] += 1
-    s.append(cnt)
-  for j in range(9):
-    cnt = [0]*10
-    for k in range(9):
-      cnt[arr[k][j]] += 1
-    s.append(cnt)
-  for x in range(0,9,3):
-    for y in range(0,9,3):
-      cnt = [0]*10
-      for a in range(3):
-        for b in range(3):
-          cnt[arr[x+a][y+b]] += 1
-      s.append(cnt)
-  result = 1
-  for c in range(27):
-    for d in range(1, 10):
-      if s[c][d] != 1:
-        result = 0
-  print(f'#{t+1} {result}')
+    cnt1 = [0]*10
+    cnt2 = [0]*10
+    for j in range(9):
+      cnt1[a[i][j]] += 1
+      if cnt1[a[i][j]] >= 2:
+        return 0
+      cnt2[a[j][i]] += 1
+      if cnt2[a[j][i]] >= 2:
+        return 0
+  for k in range(0,9,3):
+    for l in range(0,9,3):
+      cnt3 = [0]*10
+      for x in range(3):
+        for y in range(3):
+          cnt3[a[k+x][l+y]] += 1
+          if cnt3[a[k+x][l+y]] >= 2:
+            return 0
+  return 1
+for t in range(int(input())):
+  a = [list(map(int,input().split())) for _ in range(9)]
+  print(f'#{t+1} {sdoku(a)}')
