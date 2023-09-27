ebc9dac45a35bbbf52684f55acddcea8a71e840f
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-27 14:24:14 +0900

    [D2] Title: 지그재그 숫자, Time: 158 ms, Memory: 58,240 KB -BaekjoonHub

--- SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
@@ -1,8 +1,8 @@
 for t in range(int(input())):
   n = int(input())
-  arr = [i for i in range(1, n+1)]
+  a = [i for i in range(1, n+1)]
   result = 0
-  for i in arr:
+  for i in a:
     if i % 2 == 0:
       result -= i
     else:
