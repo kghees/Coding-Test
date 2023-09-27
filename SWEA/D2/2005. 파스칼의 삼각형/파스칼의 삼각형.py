21f49eddda5880105615d1d659ed6455925dd273
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-27 11:11:09 +0900

    [D2] Title: 파스칼의 삼각형, Time: 104 ms, Memory: 43,776 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,14 +1,13 @@
 for t in range(int(input())):
   n = int(input())
-  result = []
-  for i in range(n):
-    ans = []
+  d = [[0]*i for i in range(1,n+1)]
+  d[0][0] = 1
+  for i in range(1,n):
     for j in range(i+1):
-      if j == 0 or j == i:
-        ans.append(1)
+      if i == j or j == 0:
+        d[i][j] = 1
       else:
-        ans.append(result[i-1][j] + result[i-1][j-1])
-    result.append(ans)
+        d[i][j] = d[i-1][j-1] + d[i-1][j]
   print(f'#{t+1}')
-  for i in result:
+  for i in d:
     print(*i)
