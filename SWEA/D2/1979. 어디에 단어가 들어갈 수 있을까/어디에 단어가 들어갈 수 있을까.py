8546570d1b71e89292cd4a49b79868f21e0d8118
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-27 11:53:53 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 109 ms, Memory: 46,116 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,25 +1,23 @@
 for t in range(int(input())):
-  n, k = map(int,input().split())
-  arr = [list(map(int,input().split())) for _ in range(n)]
-  result = 0
+  n,k = map(int,input().split())
+  a = [list(map(int,input().split())) for _ in range(n)]
+  cnt = 0
   for i in range(n):
-    cnt = 0
+    x = 0
+    y = 0
     for j in range(n):
-      if arr[i][j] == 1:
-        cnt += 1
-      if arr[i][j] == 0 or j == n-1:
-        if cnt == k:
-          result += 1
-        if arr[i][j] == 0:
-          cnt = 0
-  for i in range(n):
-    cnt = 0
-    for j in range(n):
-      if arr[j][i] == 1:
-        cnt += 1
-      if arr[j][i] == 0 or j == n-1:
-        if cnt == k:
-          result += 1
-      if arr[j][i] == 0:
-        cnt = 0
-  print(f'#{t+1} {result}')
+      if a[i][j] == 1:
+        x += 1
+      if a[i][j] == 0 or j == n-1:
+        if x == k:
+          cnt += 1
+        if a[i][j] == 0:
+          x = 0
+      if a[j][i] == 1:
+        y += 1
+      if a[j][i] == 0 or j == n-1:
+        if y == k:
+          cnt += 1
+        if a[j][i] == 0:
+          y = 0
+  print(f'#{t+1} {cnt}')
