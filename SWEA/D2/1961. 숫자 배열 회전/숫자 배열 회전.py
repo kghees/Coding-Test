377196b6619708b5ce142bed9ea07f357687fdad
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-27 14:14:15 +0900

    [D2] Title: 숫자 배열 회전, Time: 153 ms, Memory: 58,784 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,22 +1,22 @@
 for t in range(int(input())):
   n = int(input())
-  arr = [list(map(int,input().split())) for _ in range(n)]
-  arr_90 = [[0 for _ in range(n)] for _ in range(n)]
-  arr_180 = [[0 for _ in range(n)] for _ in range(n)]
-  arr_270 = [[0 for _ in range(n)] for _ in range(n)]
+  a = [list(map(int,input().split())) for _ in range(n)]
+  a_90 =[[0]*n for _ in range(n)]
+  a_180 = [[0]*n for _ in range(n)]
+  a_270 = [[0]*n for _ in range(n)]
   for i in range(n):
     for j in range(n):
-      arr_90[i][j] = arr[n-j-1][i]
-      arr_180[i][j] = arr[n-i-1][n-j-1]
-      arr_270[i][j] = arr[j][n-i-1]
+      a_90[i][j] = a[n-j-1][i]
+      a_180[i][j] = a[n-i-1][n-j-1]
+      a_270[i][j] = a[j][n-i-1]
   print(f'#{t+1}')
-  for x in range(n):
-    for a in range(n):
-      print(arr_90[x][a],end='')
+  for i in range(n):
+    for j in range(n):
+      print(a_90[i][j],end='')
     print(end=' ')
-    for b in range(n):
-      print(arr_180[x][b],end='')
+    for k in range(n):
+      print(a_180[i][k],end='')
     print(end=' ')
-    for c in range(n):
-      print(arr_270[x][c],end='')
+    for l in range(n):
+      print(a_270[i][l],end='')
     print()
