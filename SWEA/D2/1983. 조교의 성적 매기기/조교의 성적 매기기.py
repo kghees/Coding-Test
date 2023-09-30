3f6b875d2037315dbc81a15f3408826d21ab240c
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-30 17:46:48 +0900

    [D2] Title: 조교의 성적 매기기, Time: 125 ms, Memory: 47,696 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,13 +1,11 @@
 for t in range(int(input())):
+  num = ['A+','A0','A-','B+','B0','B-','C+','C0','C-','D0']
   n, k = map(int,input().split())
-  score = ['A+','A0','A-','B+','B0','B-','C+','C0','C-','D0']
-  arr = []
+  result = []
   for _ in range(n):
     a,b,c = map(int,input().split())
-    value = (a*0.35) + (b*0.45) + (c*0.20)
-    arr.append(value)
-  k_score = arr[k-1]
-  arr.sort(reverse = True)
-  x = n // 10
-  result = arr.index(k_score)//x
-  print(f'#{t+1} {score[result]}')
+    ans = result.append((a*0.35)+(b*0.45)+(c*0.2))
+  x = result[k-1]
+  result.sort(reverse=True)
+  z = n//10
+  print(f'#{t+1} {num[result.index(x)//z]}')
