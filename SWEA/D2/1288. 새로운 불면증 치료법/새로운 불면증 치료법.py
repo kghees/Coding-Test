244af6a300c4d932be5cf2a68a87f328e904faf3
김건희 <92205960+kghees@users.noreply.github.com>
2023-09-30 19:59:00 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 118 ms, Memory: 52,288 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,10 +1,10 @@
 for t in range(int(input())):
   n = int(input())
   num = [0]*10
-  a = 1
+  cnt = 1
   while 0 in num:
-    ans = str(n*a)
-    for i in ans:
+    x = str(cnt * n)
+    for i in x:
       num[int(i)] += 1
-    a += 1
-  print(f'#{t+1} {(a-1)*n}')
+    cnt += 1
+  print(f'#{t+1} {(cnt-1)*n}')
