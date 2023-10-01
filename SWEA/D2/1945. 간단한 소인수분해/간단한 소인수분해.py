1e73e5972f4f14b22ae3ea9f9e6d79cfe240ede6
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-01 15:57:47 +0900

    [D2] Title: 간단한 소인수분해, Time: 148 ms, Memory: 58,508 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,20 +1,20 @@
 for t in range(int(input())):
   n = int(input())
   a,b,c,d,e = 0,0,0,0,0
-  while n!=1:
+  while n != 1:
     if n % 2 == 0:
+      n //= 2
       a += 1
-      n /= 2
     elif n % 3 == 0:
+      n //= 3
       b += 1
-      n /= 3
     elif n % 5 == 0:
+      n //= 5
       c += 1
-      n /= 5
     elif n % 7 == 0:
+      n //= 7
       d += 1
-      n /= 7
     elif n % 11 == 0:
+      n //= 11
       e += 1
-      n /= 11
-  print(f'#{t+1} {a} {b} {c} {d} {e}')
+  print(f'#{t+1} {a} {b} {c} {d} {e}') 
