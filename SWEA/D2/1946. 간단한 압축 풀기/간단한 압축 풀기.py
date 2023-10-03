d7a72c247253a4686e2404660b06ffd6ec047e60
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-03 16:30:10 +0900

    [D2] Title: 간단한 압축 풀기, Time: 150 ms, Memory: 58,776 KB -BaekjoonHub

--- SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
+++ SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
@@ -2,8 +2,8 @@ for t in range(int(input())):
   n = int(input())
   arr = ''
   for _ in range(n):
-    c, k = input().split()
-    arr += c*int(k)
+    x,y = input().split()
+    arr += x*int(y)
   print(f'#{t+1}')
   for i in range(0,len(arr),10):
     print(arr[i:i+10])
