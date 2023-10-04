897ec48975b4ad507f449228c477db94f3ea9455
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-04 20:53:26 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 106 ms, Memory: 46,128 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,17 +1,14 @@
-for t in range(10):
+for t in range(1,11):
   n = int(input())
-  code = [list(input()) for _ in range(8)]
+  a = [input() for _ in range(8)]
   cnt = 0
   for i in range(8):
     for j in range(8-n+1):
-      num = code[i][j:j+n]
-      if num == num[::-1]:
+      if a[i][j:j+n] == a[i][j:j+n][::-1]:
         cnt += 1
-  for i in range(8):
-    for j in range(8-n+1):
       ans = ''
       for k in range(n):
-        ans += code[j+k][i]
+        ans += a[j+k][i]
       if ans == ans[::-1]:
         cnt += 1
-  print(f'#{t+1} {cnt}')
+  print(f'#{t} {cnt}')
