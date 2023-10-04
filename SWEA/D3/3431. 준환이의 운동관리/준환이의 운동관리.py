30cf039953ae9efe43997f7149c4032029c71e03
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-04 14:06:13 +0900

    [D3] Title: 준환이의 운동관리, Time: 320 ms, Memory: 62,772 KB -BaekjoonHub

--- SWEA/D3/3431. 준환이의 운동관리/준환이의 운동관리.py
+++ SWEA/D3/3431. 준환이의 운동관리/준환이의 운동관리.py
@@ -1,10 +1,8 @@
 for t in range(int(input())):
   l,u,x = map(int,input().split())
-  result = 0
   if l > x:
-    result = l-x
+    print(f'#{t+1} {l-x}')
   elif l <= x <= u:
-    result = 0
+    print(f'#{t+1} {0}')
   else:
-    result = -1
-  print(f'#{t+1} {result}')
+    print(f'#{t+1} {-1}')
