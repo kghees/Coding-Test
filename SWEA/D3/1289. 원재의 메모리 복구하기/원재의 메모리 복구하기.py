5fdd5343f8db9f415269c5becac14866f4a6cc44
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-04 14:58:45 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 150 ms, Memory: 58,504 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -1,9 +1,9 @@
 for t in range(int(input())):
-  m = input()
-  cnt = 0
+  n = input()
   x = '0'
-  for i in range(len(m)):
-    if m[i] != x:
-      x = m[i]
+  cnt = 0
+  for i in n:
+    if x != i:
+      x = i
       cnt += 1
   print(f'#{t+1} {cnt}')
