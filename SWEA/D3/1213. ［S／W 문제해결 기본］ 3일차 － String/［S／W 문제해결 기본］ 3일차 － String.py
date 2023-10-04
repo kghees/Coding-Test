6233a1df6bff5788b39f30c5f7a95a9e11aff80b
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-04 23:36:52 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 107 ms, Memory: 45,652 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
@@ -1,9 +1,9 @@
-for t in range(10):
+for t in range(1,11):
   n = int(input())
   s = input()
-  string = input()
+  x = input()
   cnt = 0
-  for i in range(len(string)-len(s)+1):
-    if s == string[i:i+len(s)]:
+  for i in range(len(x)-len(s)+1):
+    if x[i:i+len(s)] == s:
       cnt += 1
-  print(f'#{t+1} {cnt}')
+  print(f'#{t} {cnt}')
