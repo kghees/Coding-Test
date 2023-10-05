5924a02a8617c32d5bb97d9f51c7352b25d99a19
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-05 16:47:42 +0900

    [D3] Title: 농작물 수확하기, Time: 177 ms, Memory: 61,448 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,15 +1,15 @@
 for t in range(int(input())):
   n = int(input())
-  farm = [list(map(int,input())) for _ in range(n)]
-  a, b = n//2 , n//2
+  a = [list(map(int,input())) for _ in range(n)]
   result = 0
+  x,y = n//2, n//2
   for i in range(n):
-    for j in range(a, b+1):
-      result += farm[i][j]
+    for j in range(x,y+1):
+      result += a[i][j]
     if i < n//2:
-      a -= 1
-      b += 1
+      x -=1
+      y += 1
     else:
-      a += 1
-      b -= 1
+      x += 1
+      y -= 1
   print(f'#{t+1} {result}')
