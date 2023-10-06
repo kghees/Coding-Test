270bbd72eb7abe499c66a888dc5b90efb1605420
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-06 19:09:59 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 514 ms, Memory: 65,340 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -1,39 +1,39 @@
-def palindrome1():
+def palindrome():
   global cnt
   for k in range(100,0,-1):
     for i in range(100):
       for j in range(100):
-        if j+k > 100:
+        if j + k > 100:
           break
-        x = j+k-1
+        x = j + k - 1
         for l in range(j,j+(k//2)):
-          if arr[i][l] != arr[i][x]:
+          if a[i][l] != a[i][x]:
             break
           x -= 1
         else:
           cnt = k
           return
-def palindrome2():
+def palindrome1():
   global cnt
   for k in range(100,0,-1):
     if cnt >= k:
       return
     for i in range(100):
-      if i + k > 100:
+      if i+k > 100:
         break
       for j in range(100):
-        x = i+k-1
+        x = i + k - 1
         for l in range(i,i+(k//2)):
-          if arr[l][j] != arr[x][j]:
+          if a[l][j] != a[x][j]:
             break
           x -= 1
         else:
           cnt = k
           return
-for t in range(10):
+for t in range(1,11):
   n = int(input())
-  arr = [list(map(str,input())) for _ in range(100)]
+  a = [list(map(str,input())) for _ in range(100)]
   cnt = 0
+  palindrome()
   palindrome1()
-  palindrome2()
-  print(f'#{t+1} {cnt}')
+  print(f'#{t} {cnt}')
