792626412d0f626baca0375743e5084ac4c7e7ff
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-07 16:20:53 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 103 ms, Memory: 46,380 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -1,16 +1,16 @@
 for t in range(int(input())):
-  n, m = map(int,input().split())
+  n,m = map(int,input().split())
   result = -1
-  password = ['1011000','1001100','1100100','1011110','1100010','1000110','1111010','1101110','1110110','1101000']
+  num = ['1011000','1001100','1100100','1011110','1100010','1000110','1111010','1101110','1110110','1101000']
   for i in range(n):
-    code = input()
-    if int(code) != 0 and result == -1:
-      code = str(int(code[::-1]))
+    s = input()
+    if int(s) != 0 and result == -1:
+      ans = str(int(s[::-1]))
       arr = []
-      for j in range(8):
-        arr.insert(0,password.index(code[j*7:j*7+7]))
-      d = (arr[0]+arr[2]+arr[4]+arr[6])*3 + arr[1]+arr[3]+arr[5]+arr[7]
-      if d % 10 == 0:
+      for k in range(8):
+        arr.insert(0,num.index(ans[k*7:k*7+7]))
+      x = (arr[0]+arr[2]+arr[4]+arr[6])*3 + (arr[1]+arr[3]+arr[5]+arr[7])
+      if x % 10 == 0:
         result = sum(arr)
       else:
         result = 0
