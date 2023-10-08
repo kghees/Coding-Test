c0d6de9e04c96592eadcb67cb5190c4703833186
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-08 20:58:00 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 206 ms, Memory: 64,096 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -1,12 +1,13 @@
+num = ['ZRO','ONE','TWO','THR','FOR','FIV','SIX','SVN','EGT','NIN']
 for t in range(int(input())):
-  a, b = input().split()
-  arr = list(input().split())
-  num = ('ZRO','ONE','TWO','THR','FOR','FIV','SIX','SVN','EGT','NIN')
-  gns = []
-  for i in range(int(b)):
-    gns.append(num.index(arr[i]))
-  gns.sort()
-  for j in range(int(b)):
-    gns[j] = num[gns[j]]
+  a,b = input().split()
+  s = list(input().split())
+  arr = []
+  for i in range(len(s)):
+    arr.append(num.index(s[i]))
+  arr.sort()
+  for i in range(len(arr)):
+    x = int(arr[i])
+    arr[i] = num[x]
   print(f'#{t+1}')
-  print(*gns)
+  print(*arr)
