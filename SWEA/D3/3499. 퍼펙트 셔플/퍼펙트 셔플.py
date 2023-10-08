370740492526279615fa56ec3e78b518524ca99a
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-08 18:43:03 +0900

    [D3] Title: 퍼펙트 셔플, Time: 174 ms, Memory: 61,740 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
@@ -1,22 +1,22 @@
 for t in range(int(input())):
   n = int(input())
   a = list(input().split())
-  shuffle = []
-  if len(a) % 2 == 0:
-    arr = a[:n//2]
-    brr = a[n//2:]
-    for i in range(n//2):
-      if len(arr) > 0:
-        shuffle.append(arr.pop(0))
-      if len(brr) > 0:
-        shuffle.append(brr.pop(0))
+  arr = []
+  brr = []
+  stack = []
+  x = 0
+  if n % 2 == 0:
+    x = n // 2
   else:
-    arr = a[:n//2+1]
-    brr = a[n//2+1:]
-    for i in range(n//2+1):
-      if len(arr) > 0:
-        shuffle.append(arr.pop(0))
-      if len(brr) > 0:
-        shuffle.append(brr.pop(0))
+    x = n//2 + 1
+  arr = a[:x]
+  brr = a[x:]
+  while True:
+    if not arr:
+      break
+    stack.append(arr.pop(0))
+    if not brr:
+      break
+    stack.append(brr.pop(0))
   print(f'#{t+1}',end=' ')
-  print(*shuffle)
+  print(*stack)
