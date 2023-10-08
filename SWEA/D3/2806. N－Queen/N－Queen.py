209766a42229e57060e05981b957323963116d01
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-08 23:03:34 +0900

    [D3] Title: N-Queen, Time: 306 ms, Memory: 64,088 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,20 +1,20 @@
-def visited(x):
+def visit(x):
   for i in range(x):
-    if row[x] == row[i] or abs(row[x]-row[i]) == x-i:
+    if check[x] == check[i] or abs(check[x]-check[i]) == x-i:
       return False
   return True
-def chess(x):
+def dfs(x):
   global result
   if x == n:
     result += 1
   else:
     for i in range(n):
-      row[x] = i
-      if visited(x):
-        chess(x+1)
+      check[x] = i
+      if visit(x):
+        dfs(x+1)
 for t in range(int(input())):
   n = int(input())
-  row = [0]*n
+  check = [0]*n
   result = 0
-  chess(0)
+  dfs(0)
   print(f'#{t+1} {result}')
