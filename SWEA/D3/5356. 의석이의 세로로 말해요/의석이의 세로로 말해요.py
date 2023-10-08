1491bb239f7382809030c9e8b5fe269230d33f18
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-08 20:29:51 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 193 ms, Memory: 61,988 KB -BaekjoonHub

--- SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
@@ -1,12 +1,12 @@
 for t in range(int(input())):
-  arr = [list(input()) for _ in range(5)]
-  s = [[0]*15 for _ in range(15)]
-  for i in range(len(arr)):
-    for j in range(len(arr[i])):
-      s[i][j] = arr[i][j]
-  word = ''
-  for i in range(15):
-    for j in range(15):
+  a = [input() for _ in range(5)]
+  s = [[0]*15 for _ in range(5)]
+  for i in range(len(a)):
+    for j in range(len(a[i])):
+      s[i][j] = a[i][j]
+  ans = ''
+  for i in range(len(s[i])):
+    for j in range(len(s)):
       if s[j][i] != 0:
-        word += s[j][i]
-  print(f'#{t+1} {word}')
+        ans += s[j][i]
+  print(f'#{t+1} {ans}')
