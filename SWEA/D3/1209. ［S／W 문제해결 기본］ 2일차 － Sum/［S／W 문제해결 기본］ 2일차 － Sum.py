a3edf9e8fa6a8575424345ecd92545eb62e1f71b
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-08 16:30:54 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 233 ms, Memory: 63,300 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,24 +1,23 @@
-for t in range(10):
+for t in range(1, 11):
   n = int(input())
-  arr = [list(map(int,input().split())) for _ in range(100)]
-  x = 0
+  a = [list(map(int,input().split())) for _ in range(100)]
+  result = 0
   for i in range(100):
-    a = sum(arr[i])
-    if a > x:
-      x = a
-  y = 0
+    if sum(a[i]) > result:
+      result = sum(a[i])
   for i in range(100):
-    b = 0
+    cnt = 0
     for j in range(100):
-      b += arr[j][i]
-    if b > y:
-      y = b
-  z = 0
-  c, d = 0, 0
+      cnt += a[j][i]
+    if result < cnt:
+      result = cnt
+  cnt1 = 0
+  cnt2 = 0
   for i in range(100):
-    c += arr[i][i]
-    d += arr[i][99-i]
-    if max(c,d) > z:
-      z = max(c,d)
-  w = max(x,y,z)
-  print(f'#{t+1} {w}')
+    cnt1 += a[i][i]
+    cnt2 += a[i][99-i]
+  if cnt1 > result:
+    result = cnt1
+  if cnt2 > result:
+    result = cnt2
+  print(f'#{t} {result}')
