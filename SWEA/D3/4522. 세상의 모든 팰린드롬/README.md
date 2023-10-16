cbd08ef1a8f36dfdd252124919a1e39468aaec9b
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-16 18:31:16 +0900

    [D3] Title: 세상의 모든 팰린드롬, Time: 96 ms, Memory: 42,980 KB -BaekjoonHub

---
+++ SWEA/D3/4522. 세상의 모든 팰린드롬/README.md
@@ -0,0 +1,15 @@
+# [D3] 세상의 모든 팰린드롬 - 4522 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWO6Oao6N4QDFAWw) 
+
+### 성능 요약
+
+메모리: 42,980 KB, 시간: 96 ms, 코드길이: 489 Bytes
+
+### 제출 일자
+
+2023-10-16 18:31
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/4522. 세상의 모든 팰린드롬/세상의 모든 팰린드롬.py
@@ -0,0 +1,23 @@
+for t in range(int(input())):
+  s = input()
+  check = True
+  if '?' not in s:
+    if s == s[::-1]:
+      print(f'#{t+1} Exist')
+    else:
+      print(f'#{t+1} Not exist')
+  else:
+    for i in range(len(s)//2):
+      if s[i] == '?':
+        continue
+      elif s[i] == s[-i-1]:
+        continue
+      elif s[i] != s[-i-1]:
+        if s[-i-1] == '?':
+          continue
+        check = False
+        break
+    if check:
+      print(f'#{t+1} Exist')
+    else:
+      print(f'#{t+1} Not exist')
