30d6584fa7166a2d7c5d8edc43da70010c7c6fb2
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-17 14:49:42 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 257 ms, Memory: 63,832 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,22 +1,22 @@
-def dfs(index,count):
+def money(x,cnt):
   global result
-  if count == int(cnt):
-    result = max(int(''.join(num)),result)
+  if int(c) == cnt:
+    result = max(int(''.join(n)),result)
     return
-  for i in range(index,len(num)):
-    for j in range(i+1,len(num)):
-      if num[i] <= num[j]:
-        num[i],num[j] = num[j],num[i]
-        dfs(i,count+1)
-        num[i],num[j] = num[j],num[i]
-  if result == 0 and int(cnt) > count:
-    a = (int(cnt) - count) % 2
+  for i in range(x,len(n)):
+    for j in range(i+1,len(n)):
+      if n[i] <= n[j]:
+        n[i],n[j] = n[j],n[i]
+        money(i,cnt+1)
+        n[i],n[j] = n[j],n[i]
+  if result == 0 and int(c) > cnt:
+    a = (int(c) - cnt) % 2
     if a == 1:
-      num[-1],num[-2] = num[-2],num[-1]
-    dfs(index,int(cnt)) 
+      n[-1],n[-2] = n[-2],n[-1]
+    money(x,int(c))    
 for t in range(int(input())):
-  num, cnt = input().split()
-  num = list(num)
+  n, c = input().split()
+  n = list(n)
   result = 0
-  dfs(0,0)
+  money(0,0)
   print(f'#{t+1} {result}')
