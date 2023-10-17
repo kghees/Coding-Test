ee3864f3fbd8d9934b91c144e81e448a745ed0cc
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-17 15:44:32 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 237 ms, Memory: 64,632 KB -BaekjoonHub

--- SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
@@ -1,40 +1,39 @@
-d = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
-for tc in range(int(input())):
-  n, m = map(int,input().split())
-  arr = [[0]*n for _ in range(n)]
-  mid = n // 2
-  arr[mid][mid] = 2
-  arr[mid-1][mid-1] = 2
-  arr[mid][mid-1] = 1
-  arr[mid-1][mid] = 1
+dx = [0,0,1,-1,1,1,-1,-1]
+dy = [1,-1,0,0,1,-1,1,-1]
+for t in range(int(input())):
+  n,m = map(int,input().split())
+  d = [[0]*n for _ in range(n)]
+  d[n//2-1][n//2-1] = 2
+  d[n//2-1][n//2] = 1
+  d[n//2][n//2-1] = 1
+  d[n//2][n//2] = 2
   for _ in range(m):
-    x,y,t = map(int,input().split())
-    x, y = x - 1, y - 1
-    data = []
-    for i in range(8):
-      dx,dy = d[i]
-      nx = dx + x
-      ny = dy + y
+    x,y,z = map(int,input().split())
+    x,y = x-1,y-1
+    ans = []
+    for k in range(8):
+      nx = x + dx[k]
+      ny = y + dy[k]
       while True:
-        if not (0<=nx<n and 0<=ny<n):
-          data = []
+        if not (0 <= nx < n and 0 <= ny < n):
+          ans = []
           break
-        if arr[nx][ny] == 0:
-          data = []
+        if d[nx][ny] == z:
           break
-        if arr[nx][ny] == t:
+        if d[nx][ny] == 0:
+          ans = []
           break
         else:
-          data.append((nx,ny))
-        nx,ny = nx+dx,ny+dy
-      for tx,ty in data:
-        if t == 1:
-          arr[tx][ty] = 1
-        elif t == 2:
-          arr[tx][ty] = 2
-      arr[x][y] = t
-    w, b = 0,0
-    for i in range(n):
-      w += arr[i].count(2)
-      b += arr[i].count(1)
-  print(f'#{tc+1} {b} {w}')    
+          ans.append((nx,ny))
+        nx,ny = nx+dx[k], ny+dy[k]
+      for i,j in ans:
+        if z == 1:
+          d[i][j] = 1
+        elif z == 2:
+          d[i][j] = 2
+      d[x][y] = z
+  w,b = 0,0
+  for i in range(n):
+    w += d[i].count(2)
+    b += d[i].count(1)
+  print(f'#{t+1} {b} {w}')
