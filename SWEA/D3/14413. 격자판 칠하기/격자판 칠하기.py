b7150f3e7d19fa569c2001fb2ece86e043e46bbc
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-19 22:04:54 +0900

    [D3] Title: 격자판 칠하기, Time: 304 ms, Memory: 64,952 KB -BaekjoonHub

--- SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.py
+++ SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.py
@@ -1,24 +1,33 @@
+from collections import deque
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
+def bfs(x,y):
+  q = deque()
+  q.append((x,y))
+  while q:
+    x,y = q.popleft()
+    for k in range(4):
+      nx = x + dx[k]
+      ny = y + dy[k]
+      if 0 <= nx < n and 0 <= ny < m:
+        if a[nx][ny] == '?':
+          if a[i][j] == '#':
+            a[nx][ny] = '.'
+          elif a[i][j] == '.':
+            a[nx][ny] = '#'
+        elif a[nx][ny] == a[i][j]:
+          return False
+  return True
 for t in range(int(input())):
   n, m = map(int,input().split())
   a = [list(input()) for _ in range(n)]
   result = 1
   for i in range(n):
     for j in range(m):
-      for k in range(4):
-        nx = i + dx[k]
-        ny = j + dy[k]
-        if 0 <= nx < n and 0 <= ny < m:
-          if a[nx][ny] == '?':
-            if a[i][j] == '#':
-              a[nx][ny] = '.'
-            elif a[i][j] == '.':
-              a[nx][ny] = '#'
-          elif a[i][j] == a[nx][ny]:
-            result = 0
-            break
-      if result == 0:
+      if bfs(i,j):
+        continue
+      else:
+        result = 0
         break
     if result == 0:
       break
