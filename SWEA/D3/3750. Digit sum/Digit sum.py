19528a1e29955b4626a3c0976791663aeda0df27
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-21 14:46:50 +0900

    [D3] Title: Digit sum, Time: 470 ms, Memory: 70,380 KB -BaekjoonHub

--- SWEA/D3/3750. Digit sum/Digit sum.py
+++ SWEA/D3/3750. Digit sum/Digit sum.py
@@ -1,9 +1,12 @@
 res = []
 t = int(input())
 for _ in range(t):
-  n = int(input())
-  while n > 9:
-    n = sum(map(int,list(str(n))))
+  n = input()
+  while len(n) != 1:
+    x = 0
+    for i in range(len(n)):
+      x += int(n[i])
+    n = str(x)
   res.append(n)
 for k in range(t):
   print(f'#{k+1} {res[k]}')
