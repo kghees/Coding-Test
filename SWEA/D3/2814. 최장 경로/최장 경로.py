62bcba9d0e448a3cbb74581254cb5d511b32f32a
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-21 16:52:38 +0900

    [D3] Title: 최장 경로, Time: 148 ms, Memory: 60,308 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,21 +1,21 @@
-def long(a,b):
-  global cnt
-  cnt = max(cnt,b)
-  for k in range(1, n+1):
-    if path[a][k] and not visited[k]:
-      visited[k] = 1
-      long(k,b+1)
-      visited[k] = 0
+def dfs(x,cnt):
+  global res
+  res = max(cnt,res)
+  for i in range(1,n+1):
+    if a[x][i] and not check[i]:
+      check[i] = True
+      dfs(i,cnt+1)
+      check[i] = False
 for t in range(int(input())):
-  n, m = map(int,input().split())
-  path = [[0 for _ in range(n+1)] for _ in range(n+1)]
-  cnt = 1
+  n,m = map(int,input().split())
+  a = [[0]*(n+1) for _ in range(n+1)]
   for _ in range(m):
-    x, y = map(int,input().split())
-    path[x][y] = 1
-    path[y][x] = 1
+    x,y = map(int,input().split())
+    a[x][y] = 1
+    a[y][x] = 1
+  res = 1
   for i in range(1, n+1):
-    visited = [0]*(n+1)
-    visited[i] = 1
-    long(i,1)
-  print(f'#{t+1} {cnt}')
+    check = [False]*(n+1)
+    check[i] = True
+    dfs(i,1)
+  print(f'#{t+1} {res}')
