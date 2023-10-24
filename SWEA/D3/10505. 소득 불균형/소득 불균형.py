c3d7ca7c91c65249152f4d6dde4e4516a5b93c55
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-24 23:18:18 +0900

    [D3] Title: 소득 불균형, Time: 260 ms, Memory: 63,308 KB -BaekjoonHub

--- SWEA/D3/10505. 소득 불균형/소득 불균형.py
+++ SWEA/D3/10505. 소득 불균형/소득 불균형.py
@@ -1,9 +1,9 @@
 for t in range(int(input())):
   n = int(input())
   a = list(map(int,input().split()))
-  avg = sum(a)//n
+  s = sum(a)//n
   cnt = 0
-  for i in range(n):
-    if avg >= a[i]:
+  for i in a:
+    if i <= s:
       cnt += 1
   print(f'#{t+1} {cnt}')
