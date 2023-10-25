d1c7a08c0f27462bd3169fb97d6d3976c4b80724
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-25 23:13:13 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 395 ms, Memory: 63,832 KB -BaekjoonHub

--- SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
@@ -1,9 +1,9 @@
 for t in range(int(input())):
   n,k = map(int,input().split())
   a = list(map(int,input().split()))
-  result = []
+  res = []
   for i in range(1, n+1):
     if i not in a:
-      result.append(i)
-  print(f'#{t+1}',end=' ')
-  print(*result)
+      res.append(i)
+  res.sort()
+  print(f'#{t+1}',*res)
