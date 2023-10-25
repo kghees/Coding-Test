4045e080e2d16d41af4b1a60ccf77be08132d5c3
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-25 23:05:01 +0900

    [D3] Title: 일요일, Time: 160 ms, Memory: 58,780 KB -BaekjoonHub

--- SWEA/D3/13229. 일요일/일요일.py
+++ SWEA/D3/13229. 일요일/일요일.py
@@ -1,7 +1,8 @@
 for t in range(int(input())):
   s = input()
-  day = ['MON','TUE','WED','THU','FRI','SAT','SUN']
-  if s == 'SUN':
-    print(f'#{t+1} {7}')
+  day = ['0','MON','TUE','WED','THU','FRI','SAT','SUN']
+  x = day.index(s)
+  if x == 7:
+    print(f'#{t+1} 7')
   else:
-    print(f'#{t+1} {7-day.index(s)-1}')
+    print(f'#{t+1} {7-x}')
