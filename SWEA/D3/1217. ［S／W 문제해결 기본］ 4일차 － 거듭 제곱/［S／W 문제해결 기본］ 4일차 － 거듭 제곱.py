1a2d6705034a630b507a092818707cad5b760c8b
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-25 20:23:23 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 144 ms, Memory: 58,512 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,10 +1,9 @@
-for t in range(1,11):
-  t = int(input())
-  n,m = map(int,input().split())
-  def dfs(n,cnt):
-    if cnt == m:
-      return n
-    else:
-      return n*dfs(n,cnt+1)
-  result = dfs(n,1)
-  print(f'#{t} {result}')
+def dfs(a,cnt):
+  if cnt == b:
+    return a
+  else:
+    return a*dfs(a,cnt+1)
+for t in range(1, 11):
+  n = int(input())
+  a,b = map(int,input().split())
+  print(f'#{t} {dfs(a,1)}')
