dbe500e7c07eaad6102281099de79284aeebdd7e
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-25 23:58:03 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 151 ms, Memory: 58,548 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.py
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.py
@@ -1,15 +1,11 @@
 for t in range(1,11):
-  n,num = input().split()
-  stack = []
-  for i in num:
-    if not stack:
-      stack.append(i)
+  a, s = input().split()
+  ans = []
+  for i in s:
+    if not ans:
+      ans.append(i)
+    elif ans and ans[-1] == i:
+      ans.pop()
     else:
-      if stack[-1] == i:
-        stack.pop()
-      else:
-        stack.append(i)
-  print(f'#{t}',end=' ')
-  for i in stack:
-    print(i,end='')
-  print()
+      ans.append(i)
+  print(f'#{t}',''.join(ans))
