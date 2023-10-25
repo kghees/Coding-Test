50f88bd0ae8afa9bf3aa87ed12c3965c4415a262
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-25 21:11:50 +0900

    [D3] Title: 제곱 팰린드롬 수, Time: 192 ms, Memory: 62,608 KB -BaekjoonHub

--- SWEA/D3/10570. 제곱 팰린드롬 수/제곱 팰린드롬 수.py
+++ SWEA/D3/10570. 제곱 팰린드롬 수/제곱 팰린드롬 수.py
@@ -2,10 +2,7 @@ for t in range(int(input())):
   a,b = map(int,input().split())
   cnt = 0
   for i in range(a, b+1):
-    x = i**(1/2)
-    if x == int(x):
-      i = str(i)
-      x = str(int(x))
-      if i == i[::-1] and x == x[::-1]:
+    if str(i) == str(i)[::-1]:
+      if i**0.5 == int(i**0.5) and str(int(i**0.5)) == str(int(i**0.5))[::-1]:
         cnt += 1
   print(f'#{t+1} {cnt}')
