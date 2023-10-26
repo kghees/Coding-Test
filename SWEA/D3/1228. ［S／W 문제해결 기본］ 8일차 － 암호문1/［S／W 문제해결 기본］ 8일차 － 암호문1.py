10a8b944eb686193dd3b4f05dea300645875c284
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-26 21:09:03 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 141 ms, Memory: 58,512 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -1,13 +1,12 @@
-for t in range(1, 11):
+for t in range(1,11):
   n = int(input())
   a = list(map(int,input().split()))
   m = int(input())
-  s = list(input().split())
-  for i in range(len(s)):
-    if s[i] == 'I':
-      x = int(s[i+1])
-      y = int(s[i+2])
-      num = list(s[i+3:i+3+y])
+  b = list(input().split())
+  for i in range(len(b)):
+    if b[i] == 'I':
+      x = int(b[i+1])
+      y = int(b[i+2])
+      num = list(b[i+3:i+3+y])
       a[x:x] = num
-  print(f'#{t}',end=' ')
-  print(*a[:10])
+  print(f'#{t}',*a[:10])
