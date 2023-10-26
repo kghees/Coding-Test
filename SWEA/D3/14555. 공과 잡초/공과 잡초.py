4e8755fcdebf0284f4357b4aee8bebf710df9632
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-26 22:30:24 +0900

    [D3] Title: 공과 잡초, Time: 188 ms, Memory: 64,356 KB -BaekjoonHub

--- SWEA/D3/14555. 공과 잡초/공과 잡초.py
+++ SWEA/D3/14555. 공과 잡초/공과 잡초.py
@@ -1,3 +1,21 @@
 for t in range(int(input())):
   s = input()
-  print(f'#{t+1} {s.count("(") + s.count(")") - s.count("()")}')
+  stack = []
+  cnt = 0
+  for i in range(len(s)):
+    if s[i] == '(':
+      stack.append('(')
+    elif s[i] == '|':
+      if stack and stack[-1] == '(':
+        stack.pop()
+        cnt += 1
+      else:
+        stack.append('|')
+    elif s[i] == ')':
+      if stack and stack[-1] == '|':
+        stack.pop()
+        cnt += 1
+      elif stack and stack[-1] == '(':
+        stack.pop()
+        cnt += 1
+  print(f'#{t+1} {cnt}')
