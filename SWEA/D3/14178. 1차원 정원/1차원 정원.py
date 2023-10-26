7a25bfa8c540c9839c311e9ca39c86bc3ef1330c
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-26 21:59:22 +0900

    [D3] Title: 1차원 정원, Time: 1,202 ms, Memory: 64,888 KB -BaekjoonHub

--- SWEA/D3/14178. 1차원 정원/1차원 정원.py
+++ SWEA/D3/14178. 1차원 정원/1차원 정원.py
@@ -1,8 +1,6 @@
 for t in range(int(input())):
   n,d = map(int,input().split())
-  x = d*2 + 1
-  result = n//x
-  if n % x == 0:
-    print(f'#{t+1} {result}')
+  if n % (d*2+1) == 0:
+    print(f'#{t+1} {n//(d*2+1)}')
   else:
-    print(f'#{t+1} {result+1}')
+    print(f'#{t+1} {n//(d*2+1)+1}')
