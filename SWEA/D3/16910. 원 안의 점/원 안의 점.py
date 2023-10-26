94d74995b21b08644972166e9674e7c45eb1c802
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-26 20:59:52 +0900

    [D3] Title: 원 안의 점, Time: 238 ms, Memory: 61,716 KB -BaekjoonHub

--- SWEA/D3/16910. 원 안의 점/원 안의 점.py
+++ SWEA/D3/16910. 원 안의 점/원 안의 점.py
@@ -1,10 +1,8 @@
 for t in range(int(input())):
   n = int(input())
-  x = [i for i in range(-n, n+1)]
-  y = [i for i in range(-n, n+1)]
   cnt = 0
-  for i in range(len(x)):
-    for j in range(len(y)):
-      if (x[i]**2) + (y[j]**2) <= n**2:
+  for i in range(-n, n+1):
+    for j in range(-n, n+1):
+      if (i**2)+(j**2) <= n**2:
         cnt += 1
   print(f'#{t+1} {cnt}')
