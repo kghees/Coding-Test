194eb0680204fe5bffcddff8307421b68a87df8d
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-26 21:46:46 +0900

    [D3] Title: 최대 성적표 만들기, Time: 174 ms, Memory: 61,484 KB -BaekjoonHub

--- SWEA/D3/4466. 최대 성적표 만들기/최대 성적표 만들기.py
+++ SWEA/D3/4466. 최대 성적표 만들기/최대 성적표 만들기.py
@@ -2,7 +2,7 @@ for t in range(int(input())):
   n,k = map(int,input().split())
   a = list(map(int,input().split()))
   a.sort(reverse=True)
-  result = 0
+  res = 0
   for i in range(k):
-    result += a[i]
-  print(f'#{t+1} {result}')
+    res += a[i]
+  print(f'#{t+1} {res}')
