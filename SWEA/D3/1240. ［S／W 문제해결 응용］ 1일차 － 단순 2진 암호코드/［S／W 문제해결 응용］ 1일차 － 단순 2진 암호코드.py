db478944e2b383c828e45ec1749cd5cbcb95664f
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-26 16:53:36 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 158 ms, Memory: 58,788 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -1,17 +1,18 @@
 for t in range(int(input())):
   n,m = map(int,input().split())
-  result = -1
+  a = [input() for _ in range(n)]
   num = ['1011000','1001100','1100100','1011110','1100010','1000110','1111010','1101110','1110110','1101000']
+  res = -1
   for i in range(n):
-    s = input()
-    if int(s) != 0 and result == -1:
-      ans = str(int(s[::-1]))
-      arr = []
-      for k in range(8):
-        arr.insert(0,num.index(ans[k*7:k*7+7]))
-      x = (arr[0]+arr[2]+arr[4]+arr[6])*3 + (arr[1]+arr[3]+arr[5]+arr[7])
+    if int(a[i]) != 0 and res == -1:
+      s = str(int(a[i][::-1]))
+      ans = []
+      for j in range(8):
+        ans.append(num.index(s[j*7:j*7+7]))
+      ans = ans[::-1]
+      x = (ans[0]+ans[2]+ans[4]+ans[6])*3 + (ans[1]+ans[3]+ans[5]+ans[7])
       if x % 10 == 0:
-        result = sum(arr)
+        res = sum(ans)
       else:
-        result = 0
-  print(f'#{t+1} {result}')
+        res = 0
+  print(f'#{t+1} {res}')
