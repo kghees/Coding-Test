29a4b6f7e1eaf5135828a69276df9918beb3ae51
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-26 22:52:31 +0900

    [D3] Title: 영준이와 신비한 뿔의 숲, Time: 164 ms, Memory: 58,768 KB -BaekjoonHub

--- SWEA/D3/3142. 영준이와 신비한 뿔의 숲/영준이와 신비한 뿔의 숲.py
+++ SWEA/D3/3142. 영준이와 신비한 뿔의 숲/영준이와 신비한 뿔의 숲.py
@@ -1,5 +1,6 @@
 for t in range(int(input())):
   n,m = map(int,input().split())
-  x = 2*m - n
-  y = n - m
-  print(f'#{t+1} {x} {y}')
+  a,b = 0,0
+  b = n-m
+  a = m-b
+  print(f'#{t+1} {a} {b}')
