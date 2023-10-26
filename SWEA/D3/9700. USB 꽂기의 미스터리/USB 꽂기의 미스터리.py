af0c0e8c46071d2cbd6313a62428e692b8a8c06b
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-26 20:34:56 +0900

    [D3] Title: USB 꽂기의 미스터리, Time: 149 ms, Memory: 58,508 KB -BaekjoonHub

--- SWEA/D3/9700. USB 꽂기의 미스터리/USB 꽂기의 미스터리.py
+++ SWEA/D3/9700. USB 꽂기의 미스터리/USB 꽂기의 미스터리.py
@@ -1,10 +1,8 @@
 for t in range(int(input())):
-  p,q = input().split()
-  p = float(p)
-  q = float(q)
-  a = (1-p)*(1-q)
-  b = p*(1-q)*(1-q)
-  if a < b:
+  p,q = map(float,input().split())
+  x = (1-p)*(1-q)
+  y = p*(1-q)*(1-q)
+  if x < y:
     print(f'#{t+1} YES')
   else:
     print(f'#{t+1} NO')
