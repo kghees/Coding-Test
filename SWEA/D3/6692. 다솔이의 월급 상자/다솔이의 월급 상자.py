f61a2af8e3d72872f9fd7fb3e4e891adbf2ad152
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-27 15:12:33 +0900

    [D3] Title: 다솔이의 월급 상자, Time: 353 ms, Memory: 64,416 KB -BaekjoonHub

--- SWEA/D3/6692. 다솔이의 월급 상자/다솔이의 월급 상자.py
+++ SWEA/D3/6692. 다솔이의 월급 상자/다솔이의 월급 상자.py
@@ -1,7 +1,7 @@
 for t in range(int(input())):
   n = int(input())
-  result = 0
+  res = 0
   for _ in range(n):
-    p,x = input().split()
-    result += float(p)*float(x)
-  print(f'#{t+1} {result:.6f}')
+    p,x = map(float,input().split())
+    res += p*x
+  print(f'#{t+1} {res:.6f}')
