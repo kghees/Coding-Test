b2245372d3fdfb08c5716efe83ba05185a7dce9a
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-29 17:17:47 +0900

    [D3] Title: 퍼펙트 셔플, Time: 158 ms, Memory: 61,992 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
@@ -1,22 +1,19 @@
 for t in range(int(input())):
   n = int(input())
-  a = list(input().split())
-  arr = []
-  brr = []
-  stack = []
-  x = 0
-  if n % 2 == 0:
-    x = n // 2
+  a = list(map(str,input().split()))
+  res = []
+  if len(a) % 2 == 0:
+    arr = a[:len(a)//2]
+    brr = a[len(a)//2:]
+    for i in range(len(a)//2):
+      res.append(arr.pop(0))
+      res.append(brr.pop(0))
   else:
-    x = n//2 + 1
-  arr = a[:x]
-  brr = a[x:]
-  while True:
-    if not arr:
-      break
-    stack.append(arr.pop(0))
-    if not brr:
-      break
-    stack.append(brr.pop(0))
-  print(f'#{t+1}',end=' ')
-  print(*stack)
+    arr = a[:len(a)//2+1]
+    brr = a[len(a)//2+1:]
+    for i in range(len(a)//2):
+      res.append(arr.pop(0))
+      res.append(brr.pop(0))
+    if arr:
+      res.append(arr.pop(0))
+  print(f'#{t+1}',*res)
