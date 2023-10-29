edef09d982d8419a57f2a6bbca757b31276a55f1
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-29 21:25:30 +0900

    [D3] Title: 늘어지는 소리 만들기, Time: 394 ms, Memory: 64,388 KB -BaekjoonHub

--- SWEA/D3/4676. 늘어지는 소리 만들기/늘어지는 소리 만들기.py
+++ SWEA/D3/4676. 늘어지는 소리 만들기/늘어지는 소리 만들기.py
@@ -3,6 +3,6 @@ for t in range(int(input())):
   h = int(input())
   a = list(map(int,input().split()))
   a.sort(reverse=True)
-  for i in a:
-    s.insert(i,'-')
-  print(f'#{t+1} {"".join(s)}')
+  for i in range(len(a)):
+    s.insert(a[i],'-')
+  print(f'#{t+1}',''.join(s))
