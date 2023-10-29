0a80ee21dbaa418a41b0f50b457db5f9d3b370b2
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-29 18:03:14 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 167 ms, Memory: 62,000 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,15 +1,16 @@
 for t in range(1,11):
   n = int(input())
   a = list(map(int,input().split()))
-  d = [-2,-1,1,2]
-  cnt = 0
-  for i in range(2,n-2):
-    arr = []
-    for k in range(4):
-      arr.append(a[i+d[k]])
-    arr.sort()
-    if arr[-1] < a[i]:
-      cnt += a[i] - arr[-1]
-    else:
-      continue
-  print(f'#{t} {cnt}')
+  res = 0
+  for i in range(2, len(a)-1):
+    check = False
+    for k in [-2,-1,1,2]:
+      if a[i] > a[i+k]:
+        check = True
+      else:
+        check = False
+        break
+    if check:
+      ans = max(a[i-2],a[i-1],a[i+1],a[i+2])
+      res += a[i] - ans
+  print(f'#{t} {res}')
