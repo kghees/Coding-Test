66b07d8a8ceb6e4e95eb0afa6211f029828c2aef
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-29 21:11:46 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 208 ms, Memory: 64,876 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -1,13 +1,11 @@
 num = ['ZRO','ONE','TWO','THR','FOR','FIV','SIX','SVN','EGT','NIN']
 for t in range(int(input())):
-  a,b = input().split()
   s = list(input().split())
-  arr = []
-  for i in range(len(s)):
-    arr.append(num.index(s[i]))
-  arr.sort()
-  for i in range(len(arr)):
-    x = int(arr[i])
-    arr[i] = num[x]
-  print(f'#{t+1}')
-  print(*arr)
+  a = list(input().split())
+  res = []
+  for i in range(len(a)):
+    res.append(num.index(a[i]))
+  res.sort()
+  for i in range(len(a)):
+    res[i] = num[res[i]]
+  print(f'#{t+1}',*res)
