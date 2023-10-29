5440b8cc025d3821e597b79fa862cee57117287a
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-29 16:09:47 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 216 ms, Memory: 63,040 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,23 +1,22 @@
-for t in range(1, 11):
+for t in range(1,11):
   n = int(input())
   a = [list(map(int,input().split())) for _ in range(100)]
-  result = 0
+  res = 0
   for i in range(100):
-    if sum(a[i]) > result:
-      result = sum(a[i])
+    x = sum(a[i])
+    if x > res:
+      res = x
   for i in range(100):
-    cnt = 0
+    w = 0
     for j in range(100):
-      cnt += a[j][i]
-    if result < cnt:
-      result = cnt
-  cnt1 = 0
-  cnt2 = 0
+      w += a[j][i]
+    if w > res:
+      res = w
+  y,z = 0,0
   for i in range(100):
-    cnt1 += a[i][i]
-    cnt2 += a[i][99-i]
-  if cnt1 > result:
-    result = cnt1
-  if cnt2 > result:
-    result = cnt2
-  print(f'#{t} {result}')
+    y += a[i][i]
+    z += a[i][99-i]
+  k = max(y,z)
+  if k > res:
+    res = k
+  print(f'#{t} {res}')
