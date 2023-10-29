0a761969b77fb790e150b8ad5da6b41280597199
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-29 23:17:06 +0900

    [D3] Title: [Professional] 건초더미, Time: 222 ms, Memory: 63,304 KB -BaekjoonHub

--- SWEA/D3/5603. ［Professional］ 건초더미/［Professional］ 건초더미.py
+++ SWEA/D3/5603. ［Professional］ 건초더미/［Professional］ 건초더미.py
@@ -1,9 +1,11 @@
 for t in range(int(input())):
   n = int(input())
   a = [int(input()) for _ in range(n)]
-  avg = sum(a) // len(a)
-  result = 0
+  x = sum(a)//len(a)
+  cnt = 0
   for i in a:
-    if i < avg:
-      result += abs(avg-i)
-  print(f'#{t+1} {result}')
+    if i > x:
+      cnt += i - x
+    else:
+      cnt += x - i
+  print(f'#{t+1} {cnt//2}')
