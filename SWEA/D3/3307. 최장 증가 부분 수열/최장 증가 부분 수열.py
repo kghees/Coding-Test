770cc31f9b2124f8f7ae93eccc3779c0afe7d34d
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-30 23:16:07 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 200 ms, Memory: 62,772 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
@@ -2,8 +2,8 @@ for t in range(int(input())):
   n = int(input())
   a = list(map(int,input().split()))
   d = [1]*n
-  for i in range(n):
-    for j in range(i):
+  for i in range(1, n):
+    for j in range(i+1):
       if a[i] > a[j]:
         d[i] = max(d[i],d[j]+1)
   print(f'#{t+1} {max(d)}')
