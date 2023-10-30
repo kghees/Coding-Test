c3c1b39498bc499eaf68af788438a4b80c3e8ef3
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-30 18:28:02 +0900

    [D3] Title: 구구단 1, Time: 115 ms, Memory: 49,800 KB -BaekjoonHub

--- SWEA/D3/12004. 구구단 1/구구단 1.py
+++ SWEA/D3/12004. 구구단 1/구구단 1.py
@@ -1,12 +1,12 @@
 for t in range(int(input())):
   n = int(input())
-  a,b = 0, 0
+  check = False
   for i in range(1, 10):
     for j in range(1, 10):
       if i*j == n:
-        a = i
-        b = j
-  if a != 0 and b != 0:
+        check = True
+        break
+  if check:
     print(f'#{t+1} Yes')
   else:
     print(f'#{t+1} No')
