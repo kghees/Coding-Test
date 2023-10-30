93c5e556d5d29fe9af90f67041972fcd189722c2
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-30 22:57:00 +0900

    [D3] Title: 구구단 걷기, Time: 497 ms, Memory: 61,508 KB -BaekjoonHub

--- SWEA/D3/16800. 구구단 걷기/구구단 걷기.py
+++ SWEA/D3/16800. 구구단 걷기/구구단 걷기.py
@@ -1,9 +1,9 @@
 for t in range(int(input())):
   n = int(input())
-  x = 1
-  for i in range(2, int(n**0.5)+1):
+  a = 1
+  for i in range(2,int(n**0.5)+1):
     if n % i == 0:
-      x = i
-  num = n//x - 1
-  x += num - 1
-  print(f'#{t+1} {x}')
+      a = i
+  b = n//a
+  res = (a-1) + (b-1)
+  print(f'#{t+1} {res}')
