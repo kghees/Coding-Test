3f8cb818156bae8f342416cc8fd0f2e57bc45009
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-30 19:51:01 +0900

    [D3] Title: 100만 이하의 모든 소수, Time: 323 ms, Memory: 71,832 KB -BaekjoonHub

--- SWEA/D3/3131. 100만 이하의 모든 소수/100만 이하의 모든 소수.py
+++ SWEA/D3/3131. 100만 이하의 모든 소수/100만 이하의 모든 소수.py
@@ -1,10 +1,10 @@
-n = 1000000
-nlist = [True]*(n+1)
-nlist[0], nlist[1] = False, False
-for i in range(2,int(n**0.5)+1):
-  if nlist[i]:
-    for j in range(i*2,n+1,i):
-      nlist[j] = False
-for i in range(n+1):
-  if nlist[i]:
+max = 1000000
+num = [True]*(max+1)
+num[0],num[1] = False,False
+for i in range(2, int(max**0.5)+1):
+  if num[i]:
+    for j in range(i*2,max+1,i):
+      num[j] = False
+for i in range(max+1):
+  if num[i]:
     print(i,end=' ')
