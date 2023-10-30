4da45e2082bc185e756c1c8f7a01b5504feaa969
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-30 18:09:09 +0900

    [D3] Title: 민정이와 광직이의 알파벳 공부, Time: 322 ms, Memory: 62,644 KB -BaekjoonHub

--- SWEA/D3/9480. 민정이와 광직이의 알파벳 공부/민정이와 광직이의 알파벳 공부.py
+++ SWEA/D3/9480. 민정이와 광직이의 알파벳 공부/민정이와 광직이의 알파벳 공부.py
@@ -1,15 +1,15 @@
-def dfs(x,ans):
+def alpha(x,ans):
   global cnt
   if x == n:
-    k = set(ans)
-    if len(k) == 26:
+    res = set(ans)
+    if len(res) == 26:
       cnt += 1
   else:
-    dfs(x+1,ans+a[x])
-    dfs(x+1,ans)
+    alpha(x+1,ans+a[x])
+    alpha(x+1,ans)
 for t in range(int(input())):
   n = int(input())
   a = [input().rstrip() for _ in range(n)]
   cnt = 0
-  dfs(0,'')
+  alpha(0,'')
   print(f'#{t+1} {cnt}')
