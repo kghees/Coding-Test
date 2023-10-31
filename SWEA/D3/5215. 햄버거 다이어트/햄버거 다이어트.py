dd18993b17a17a9251ca324c6c3a96d9372f009a
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-31 21:41:59 +0900

    [D3] Title: 햄버거 다이어트, Time: 292 ms, Memory: 67,268 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,17 +1,14 @@
-for tc in range(int(input())):
-  n, l = map(int,input().split())
-  a = [[0,0]]
-  for _ in range(n):
-    t,k = map(int,input().split())
-    a.append((t,k))
-  d = [[0]*(l+1) for _ in range(n+1)]
-  for i in range(1, n+1):
-    for j in range(1,l+1):
+for t in range(int(input())):
+  n,l = map(int,input().split())
+  a = [list(map(int,input().split())) for _ in range(n)]
+  d = [[0]*l for _ in range(n)]
+  for i in range(n):
+    for j in range(l):
       score = a[i][0]
       cal = a[i][1]
       if j < cal:
         d[i][j] = d[i-1][j]
       else:
-        d[i][j] = max(d[i-1][j],d[i-1][j-cal]+score)
-  result = max([max(i) for i in d])
-  print(f'#{tc+1} {result}')
+        d[i][j] = max(d[i-1][j], d[i-1][j-cal]+score)
+  res = max([max(i) for i in d])
+  print(f'#{t+1} {res}')
