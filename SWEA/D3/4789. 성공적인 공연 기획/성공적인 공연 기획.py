3cc9c725f95a7d9c292ae59ac90459e85c113bf3
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-31 20:05:16 +0900

    [D3] Title: 성공적인 공연 기획, Time: 179 ms, Memory: 61,712 KB -BaekjoonHub

--- SWEA/D3/4789. 성공적인 공연 기획/성공적인 공연 기획.py
+++ SWEA/D3/4789. 성공적인 공연 기획/성공적인 공연 기획.py
@@ -1,15 +1,15 @@
 for t in range(int(input())):
   a = input()
-  cnt = 0
-  cnt += int(a[0])
-  result = 0
-  for i in range(1, len(a)):
-    if a[i] == '0':
+  cnt = int(a[0])
+  res = 0
+  x = 0
+  for i in range(1,len(a)):
+    if a[i] == 0:
       continue
     else:
-      if cnt >= i:
-        cnt += int(a[i])
-      else:
-        result += i - cnt
+      if i > cnt:
+        res += i - cnt
         cnt = i + int(a[i])
-  print(f'#{t+1} {result}')
+      else:
+        cnt += int(a[i])
+  print(f'#{t+1} {res}')
