806f9f5b8fa9170c507c6684337f73c3ba6c591e
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-31 15:26:43 +0900

    [D3] Title: 문자열문자열, Time: 154 ms, Memory: 58,552 KB -BaekjoonHub

--- SWEA/D3/17319. 문자열문자열/문자열문자열.py
+++ SWEA/D3/17319. 문자열문자열/문자열문자열.py
@@ -1,8 +1,8 @@
 for t in range(int(input())):
   n = int(input())
   s = input()
-  x = len(s)//2
-  if s[:x] == s[x:]:
+  x = s[:len(s)//2]
+  if x*2 == s:
     print(f'#{t+1} Yes')
   else:
     print(f'#{t+1} No')
