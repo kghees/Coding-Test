4131e3bda78a3ac8d0255d631016ae0924e06a48
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-31 15:12:11 +0900

    [D3] Title: 전봇대, Time: 351 ms, Memory: 64,352 KB -BaekjoonHub

--- SWEA/D3/10580. 전봇대/전봇대.py
+++ SWEA/D3/10580. 전봇대/전봇대.py
@@ -1,12 +1,12 @@
 for t in range(int(input())):
   n = int(input())
-  result = 0
-  ans = []
-  for i in range(n):
+  res = []
+  cnt = 0
+  for _ in range(n):
     a,b = map(int,input().split())
-    if ans:
-      for x,y in ans:
+    if res:
+      for x,y in res:
         if (x > a and y < b) or (x < a and y > b):
-          result += 1
-    ans.append((a,b))
-  print(f'#{t+1} {result}')
+          cnt += 1
+    res.append((a,b))
+  print(f'#{t+1} {cnt}')
