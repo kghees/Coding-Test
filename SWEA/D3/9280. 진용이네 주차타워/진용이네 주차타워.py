6a7a8ffbe0b17396a93d1c74c11c97107eef1577
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-31 14:33:17 +0900

    [D3] Title: 진용이네 주차타워, Time: 303 ms, Memory: 67,008 KB -BaekjoonHub

--- SWEA/D3/9280. 진용이네 주차타워/진용이네 주차타워.py
+++ SWEA/D3/9280. 진용이네 주차타워/진용이네 주차타워.py
@@ -1,25 +1,25 @@
 for t in range(int(input())):
-  n, m = map(int,input().split())
-  r = [int(input()) for _ in range(n)]
-  w = [int(input()) for _ in range(m)]
-  result = 0
-  wait = []
+  n,m = map(int,input().split())
+  money = [int(input()) for _ in range(n)]
+  weight = [int(input()) for _ in range(m)]
   park = [0]*n
-  s = [int(input()) for _ in range(2*m)]
-  for i in s:
+  wait = []
+  res = 0
+  p = [int(input()) for _ in range(2*m)]
+  for i in p:
     if i < 0:
-      k = park.index(abs(i))
+      index = park.index(abs(i))
       if wait:
-        c = wait.pop(0)
-        park[k] = c
-        result += r[k]*w[c-1]
+        a = wait.pop(0)
+        park[index] = a
+        res += money[index]*weight[a-1]
       else:
-        park[k] = 0
+        park[index] = 0
     else:
       if 0 not in park:
         wait.append(i)
       else:
-        k = park.index(0)
-        park[k] = i
-        result += r[k]*w[i-1]
-  print(f'#{t+1} {result}')
+        index = park.index(0)
+        park[index] = i
+        res += money[index]*weight[i-1]
+  print(f'#{t+1} {res}')
