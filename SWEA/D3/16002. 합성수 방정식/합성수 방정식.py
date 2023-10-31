31eb6449398c6628de57d72265cc04313183db05
김건희 <92205960+kghees@users.noreply.github.com>
2023-10-31 22:25:02 +0900

    [D3] Title: 합성수 방정식, Time: 157 ms, Memory: 58,504 KB -BaekjoonHub

--- SWEA/D3/16002. 합성수 방정식/합성수 방정식.py
+++ SWEA/D3/16002. 합성수 방정식/합성수 방정식.py
@@ -1,5 +1,12 @@
+def check(x):
+  for i in range(2, int(x**0.5)+1):
+    if x % i == 0:
+      return False
+  return True
 for t in range(int(input())):
   n = int(input())
-  x = n*10
-  y = n*9
-  print(f'#{t+1} {x} {y}')
+  for i in range(n,1000000001):
+    if not check(i):
+      if (i-n) > 3 and not check(i-n):
+        print(f'#{t+1} {i} {i-n}')
+        break
