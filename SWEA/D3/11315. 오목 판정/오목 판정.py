c8dd75e8bd83511d05be7bcf45d1632b1df70270
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-01 20:05:38 +0900

    [D3] Title: 오목 판정, Time: 200 ms, Memory: 60,656 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,20 +1,31 @@
-dx = [1,1,0,1]
-dy = [0,-1,1,1]
-def omok():
+dx = [0,1,1,1]
+dy = [1,0,-1,1]
+def omok(x,y):
+  cnt = 1
+  for k in range(4):
+    nx = x + dx[k]
+    ny = y + dy[k]
+    while 0 <= nx < n and 0 <= ny < n and a[nx][ny] == 'o':
+      cnt += 1
+      nx += dx[k]
+      ny += dy[k]
+    if cnt >= 5:
+      return True
+    else:
+      cnt = 1
+  return False
+for t in range(int(input())):
+  n = int(input())
+  a = [list(input()) for _ in range(n)]
+  check = False
   for i in range(n):
     for j in range(n):
       if a[i][j] == 'o':
-        for k in range(4):
-          x,y = i,j
-          cnt = 0
-          while 0 <= x < n and 0 <= y < n and a[x][y] == 'o':
-            cnt += 1
-            x += dx[k]
-            y += dy[k]
-          if cnt >= 5:
-            return 'YES'
-  return 'NO'
-for t in range(int(input())):
-  n = int(input())
-  a = [input() for _ in range(n)]
-  print(f'#{t+1} {omok()}')
+        if omok(i,j):
+          check = True
+        else:
+          continue
+  if check:
+    print(f'#{t+1} YES')
+  else:
+    print(f'#{t+1} NO')
