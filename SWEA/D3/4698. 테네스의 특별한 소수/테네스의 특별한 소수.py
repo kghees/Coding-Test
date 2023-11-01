a14fbdcf5a86fb7b7c561db015ceacfa8ffbc7bd
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-01 11:04:43 +0900

    [D3] Title: 테네스의 특별한 소수, Time: 464 ms, Memory: 67,416 KB -BaekjoonHub

--- SWEA/D3/4698. 테네스의 특별한 소수/테네스의 특별한 소수.py
+++ SWEA/D3/4698. 테네스의 특별한 소수/테네스의 특별한 소수.py
@@ -1,16 +1,15 @@
-n = 1000000
-nlist = [True]*(n+1)
-nlist[0], nlist[1] = False, False
-for i in range(2,int(n**0.5)+1):
-  if nlist[i]:
-    for j in range(i*2,n+1,i):
-      nlist[j] = False
+max = 1000000
+num = [True]*(max+1)
+num[0],num[1] = False,False
+for i in range(2, int(max**0.5)+1):
+  if num[i]:
+    for j in range(i*2,max+1,i):
+      num[j] = False
 for t in range(int(input())):
   d,a,b = map(int,input().split())
   cnt = 0
   for i in range(a, b+1):
-    if nlist[i]:
-      x = str(i)
-      if str(d) in x:
+    if num[i]:
+      if str(d) in str(i):
         cnt += 1
   print(f'#{t+1} {cnt}')
