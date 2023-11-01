4cc23f1b4b18a48648a92111c913e28c24e09157
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-01 11:40:45 +0900

    [D3] Title: 원재의 벽 꾸미기, Time: 842 ms, Memory: 56,712 KB -BaekjoonHub

--- SWEA/D3/1491. 원재의 벽 꾸미기/원재의 벽 꾸미기.py
+++ SWEA/D3/1491. 원재의 벽 꾸미기/원재의 벽 꾸미기.py
@@ -1,13 +1,13 @@
 for t in range(int(input())):
   n,a,b = map(int,input().split())
-  m = -1
+  res = -1
   for r in range(1, n+1):
     c = 1
-    while r*c <= n:
-      value = a*abs(r-c) + b*(n-r*c)
-      if m == -1:
-        m = value
+    while r*c<=n:
+      x = a*abs(r-c) + b*(n-r*c)
+      if res == -1:
+        res = x
       else:
-        m = min(m,value)
+        res = min(x,res)
       c += 1
-  print(f'#{t+1} {m}')
+  print(f'#{t+1} {res}')
