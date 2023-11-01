da0f15c706c41ccc242459d994071c72933cffd1
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-01 20:45:07 +0900

    [D3] Title: 그래프의 삼각형, Time: 350 ms, Memory: 61,692 KB -BaekjoonHub

--- SWEA/D3/6057. 그래프의 삼각형/그래프의 삼각형.py
+++ SWEA/D3/6057. 그래프의 삼각형/그래프의 삼각형.py
@@ -1,15 +1,14 @@
-from collections import defaultdict
 for t in range(int(input())):
   n,m = map(int,input().split())
-  a = defaultdict(list)
+  a = [[] for _ in range(n+1)]
   for _ in range(m):
-    u,v = map(int,input().split())
-    a[u].append(v)
-    a[v].append(u)
-  result = 0
-  for i in range(1, n+1):
-    for j in range(i+1, n+1):
-      for k in range(j+1, n+1):
+    x,y = map(int,input().split())
+    a[x].append(y)
+    a[y].append(x)
+  cnt = 0
+  for i in range(1,n+1):
+    for j in range(i+1,n+1):
+      for k in range(j+1,n+1):
         if i in a[j] and j in a[k] and k in a[i]:
-          result += 1
-  print(f'#{t+1} {result}')
+          cnt += 1
+  print(f'#{t+1} {cnt}')
