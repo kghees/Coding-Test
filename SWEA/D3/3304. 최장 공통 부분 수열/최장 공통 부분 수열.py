bc0d66a1b0090ad51ac0d7d3d7febd0f7206d40a
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-01 15:53:41 +0900

    [D3] Title: 최장 공통 부분 수열, Time: 357 ms, Memory: 91,032 KB -BaekjoonHub

--- SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.py
+++ SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.py
@@ -1,11 +1,11 @@
 for t in range(int(input())):
-  a, b = input().split()
+  a,b = input().split()
   x,y = len(a),len(b)
   d = [[0]*(y+1) for _ in range(x+1)]
   for i in range(1, x+1):
     for j in range(1, y+1):
       if a[i-1] == b[j-1]:
-        d[i][j] = d[i-1][j-1] + 1
+        d[i][j] = d[i-1][j-1]+1
       else:
         d[i][j] = max(d[i-1][j],d[i][j-1])
   print(f'#{t+1} {d[x][y]}')
