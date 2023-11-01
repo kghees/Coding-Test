d1fc5f577688ed9806f500c4c9154dfd71e73e6a
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-01 22:03:03 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 189 ms, Memory: 61,236 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,22 +1,22 @@
 def money(x,cnt):
-  global result
-  if int(c) == cnt:
-    result = max(int(''.join(n)),result)
+  global res
+  if cnt == int(n):
+    res = max(int(''.join(a)),res)
     return
-  for i in range(x,len(n)):
-    for j in range(i+1,len(n)):
-      if n[i] <= n[j]:
-        n[i],n[j] = n[j],n[i]
+  for i in range(x,len(a)):
+    for j in range(i+1,len(a)):
+      if a[i] <= a[j]:
+        a[i],a[j] = a[j],a[i]
         money(i,cnt+1)
-        n[i],n[j] = n[j],n[i]
-  if result == 0 and int(c) > cnt:
-    a = (int(c) - cnt) % 2
-    if a == 1:
-      n[-1],n[-2] = n[-2],n[-1]
-    money(x,int(c))    
+        a[i],a[j] = a[j],a[i]
+  if res == 0 and int(n) > cnt:
+    k = (int(n) - cnt) % 2
+    if k == 1:
+      a[-1],a[-2] = a[-2],a[-1]
+    money(x,int(n))
 for t in range(int(input())):
-  n, c = input().split()
-  n = list(n)
-  result = 0
+  a, n = input().split()
+  a = list(a)
+  res = 0
   money(0,0)
-  print(f'#{t+1} {result}')
+  print(f'#{t+1} {res}')
