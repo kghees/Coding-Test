93bcac62056d19d96ef0541b14304588da48fe37
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 16:08:02 +0900

    [D3] Title: 0/1 Knapsack, Time: 251 ms, Memory: 65,684 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
@@ -1,17 +1,17 @@
 for t in range(int(input())):
-  n,k = map(int,input().split())
+  n, k = map(int,input().split())
   a = [[0,0]]
-  d = [[0]*(k+1) for _ in range(n+1)]
   for _ in range(n):
-    v,c = map(int,input().split())
-    a.append((v,c))
-  for i in range(1, n+1):
-    for j in range(1, k+1):
-      weight = a[i][0]
-      value = a[i][1]
-      if j < weight:
+    x,y = map(int,input().split())
+    a.append((x,y))
+  d = [[0]*(k+1) for _ in range(n+1)]
+  for i in range(1,n+1):
+    for j in range(1,k+1):
+      v = a[i][0]
+      c = a[i][1]
+      if v > j:
         d[i][j] = d[i-1][j]
       else:
-        d[i][j] = max(d[i-1][j], d[i-1][j-weight] + value)
-  result = max([max(i) for i in d])
-  print(f'#{t+1} {result}')
+        d[i][j] = max(d[i-1][j], d[i-1][j-v]+c)
+  res = max([max(i) for i in d])
+  print(f'#{t+1} {res}')
