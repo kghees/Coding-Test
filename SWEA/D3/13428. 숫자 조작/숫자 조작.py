480753b7fc3e17e60fdd71bc9bbcc3a485408562
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 20:46:48 +0900

    [D3] Title: 숫자 조작, Time: 178 ms, Memory: 61,744 KB -BaekjoonHub

--- SWEA/D3/13428. 숫자 조작/숫자 조작.py
+++ SWEA/D3/13428. 숫자 조작/숫자 조작.py
@@ -1,10 +1,10 @@
 from itertools import combinations
 for t in range(int(input())):
   n = list(input())
-  temp = [i for i in range(len(n))]
+  ans = [i for i in range(len(n))]
   m1,m2 = int(''.join(n)), int(''.join(n))
-  for value in combinations(temp,2):
-    i,j = value
+  for x in combinations(ans,2):
+    i,j = x
     n[i],n[j] = n[j],n[i]
     if n[0] == '0':
       n[i],n[j] = n[j],n[i]
