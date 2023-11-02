d629c665f57336c0b4003b708ff3c068b1be4fb0
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 16:48:06 +0900

    [D3] Title: 체스판 위의 룩 배치, Time: 180 ms, Memory: 61,448 KB -BaekjoonHub

--- SWEA/D3/15612. 체스판 위의 룩 배치/체스판 위의 룩 배치.py
+++ SWEA/D3/15612. 체스판 위의 룩 배치/체스판 위의 룩 배치.py
@@ -1,22 +1,39 @@
-def chess():
-  row = [0,0,0,0,0,0,0,0]
-  col = [0,0,0,0,0,0,0,0]
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+def chess(x,y):
   cnt = 0
-  for i in range(8):
-    for j in range(8):
-      if a[i][j] == 'O':
-        row[i] += 1
-        col[j] += 1
+  for k in range(4):
+    nx = x + dx[k]
+    ny = y + dy[k]
+    while True:
+      if not (0 <= nx < 8 and 0 <= ny < 8):
+        break
+      if a[nx][ny] == 'O':
+        break
+      if a[nx][ny] == '.':
         cnt += 1
-      if row[i] >= 2 or col[j] >= 2:
-        return False
-  if cnt == 8:
+      nx += dx[k]
+      ny += dy[k]
+  if cnt == 14:
     return True
   else:
     return False
 for t in range(int(input())):
-  a = [input() for _ in range(8)]
-  if chess():
+  a = [list(input()) for _ in range(8)]
+  res = 0
+  check = True
+  for i in range(8):
+    for j in range(8):
+      if a[i][j] == 'O':
+        res += 1
+        if chess(i,j):
+          continue
+        else:
+          check = False
+          break
+    if not check:
+      break
+  if check and res == 8:
     print(f'#{t+1} yes')
   else:
     print(f'#{t+1} no')
