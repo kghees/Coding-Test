dcc485963dabdfd95b21e3a8a2fe69a578cb00c3
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 18:07:19 +0900

    [D3] Title: 통역사 성경이, Time: 162 ms, Memory: 61,456 KB -BaekjoonHub

--- SWEA/D3/7675. 통역사 성경이/통역사 성경이.py
+++ SWEA/D3/7675. 통역사 성경이/통역사 성경이.py
@@ -1,17 +1,17 @@
-d = ['.','!','?']
+d = ['.','?','!']
 for t in range(int(input())):
   n = int(input())
   res = []
   cnt = 0
-  i = 0
-  while i < n:
+  x = 0
+  while x < n:
     s = input().split()
-    for x in s:
-      x1 = x.rstrip('.?!')
-      if (len(x1) == 1 and x1.isupper()) or (x1[0].isupper() and x1.isalpha() and x1[1:].islower()):
+    for i in s:
+      c = i.rstrip('.!?')
+      if (len(c) == 1 and c.isupper()) or (c[0].isupper() and c.isalpha() and c[1:].islower()):
         cnt += 1
-      if x[-1] in d:
+      if i[-1] in d:
         res.append(cnt)
+        x += 1
         cnt = 0
-        i += 1
   print(f'#{t+1}',*res)
