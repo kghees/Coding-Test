805323e660f508b568f8f8f1f4495672469deebf
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 22:23:21 +0900

    [D3] Title: 최장 경로, Time: 182 ms, Memory: 62,788 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,21 +1,21 @@
-def dfs(x,cnt):
-  global res
-  res = max(cnt,res)
+def dfs(x,k):
+  global cnt
+  cnt = max(cnt,k)
   for i in a[x]:
     if not check[i]:
       check[i] = True
-      dfs(i,cnt+1)
+      dfs(i,k+1)
       check[i] = False
 for t in range(int(input())):
   n,m = map(int,input().split())
   a = [[] for _ in range(n+1)]
   for _ in range(m):
-    x,y = map(int,input().split())
-    a[x].append(y)
-    a[y].append(x)
-  res = 1
+    u,v = map(int,input().split())
+    a[u].append(v)
+    a[v].append(u)
+  cnt = 1
   for i in range(1,n+1):
     check = [False]*(n+1)
     check[i] = True
     dfs(i,1)
-  print(f'#{t+1} {res}')
+  print(f'#{t+1} {cnt}')
