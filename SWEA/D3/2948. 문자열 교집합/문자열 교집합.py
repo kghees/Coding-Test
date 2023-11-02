60cdd24000440b2f1db7fc015b429272f051dd52
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 22:50:17 +0900

    [D3] Title: 문자열 교집합, Time: 1,804 ms, Memory: 204,892 KB -BaekjoonHub

--- SWEA/D3/2948. 문자열 교집합/문자열 교집합.py
+++ SWEA/D3/2948. 문자열 교집합/문자열 교집합.py
@@ -2,7 +2,7 @@ for t in range(int(input())):
   n,m = map(int,input().split())
   a = list(input().split())
   b = list(input().split())
-  x = n + m
-  c = a + b
-  res = x - len(set(c))
+  x = n+m
+  k = a+b
+  res = x - len(set(k))
   print(f'#{t+1} {res}')
