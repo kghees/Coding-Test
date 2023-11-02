6051a729abf27bee5a341ffefb0c36aef31078f4
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 20:00:43 +0900

    [D3] Title: 격자판 칠하기, Time: 294 ms, Memory: 64,956 KB -BaekjoonHub

--- SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.py
+++ SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.py
@@ -1,17 +1,22 @@
+from collections import deque
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
-def paint(x,y):
-  for k in range(4):
-    nx = x + dx[k]
-    ny = y + dy[k]
-    if 0 <= nx < n and 0 <= ny < m:
-      if a[nx][ny] == '?':
-        if a[x][y] == '#':
-          a[nx][ny] = '.'
-        elif a[x][y] == '.':
-          a[nx][ny] = '#'
-      elif a[x][y] == a[nx][ny]:
-        return False
+def bfs(x,y):
+  q = deque()
+  q.append((x,y))
+  while q:
+    x,y = q.popleft()
+    for k in range(4):
+      nx = x + dx[k]
+      ny = y + dy[k]
+      if 0 <= nx < n and 0 <= ny < m:
+        if a[nx][ny] == '?':
+          if a[x][y] == '#':
+            a[nx][ny] = '.'
+          elif a[x][y] == '.':
+            a[nx][ny] = '#'
+        elif a[nx][ny] == a[x][y]:
+          return False
   return True
 for t in range(int(input())):
   n,m = map(int,input().split())
@@ -19,7 +24,7 @@ for t in range(int(input())):
   check = True
   for i in range(n):
     for j in range(m):
-      if paint(i,j):
+      if bfs(i,j):
         continue
       else:
         check = False
