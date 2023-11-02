6ea045137bb4a051cfe4477901fb2b293efeffcc
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 21:08:53 +0900

    [D3] Title: 등차수열 만들기, Time: 190 ms, Memory: 58,792 KB -BaekjoonHub

--- SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.py
+++ SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.py
@@ -1,11 +1,4 @@
 for t in range(int(input())):
   a,b,c = map(int,input().split())
-  x = 0
-  if b-a == c-b:
-    x = 0
-  else:
-    if b-a > c-b:
-      x = ((b-a)-(c-b))/2
-    else:
-      x = ((c-b)-(b-a))/2
-  print(f'#{t+1} {x:.1f}')
+  res = abs(b-(a+c)/2)
+  print(f'#{t+1} {res:.1f}')
