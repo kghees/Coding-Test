f5afbeef7eae1edabd8e66432ceeb1a46b69c240
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 15:33:05 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 231 ms, Memory: 64,908 KB -BaekjoonHub

--- SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
@@ -1,12 +1,12 @@
-dx = [0,0,1,-1,1,1,-1,-1]
-dy = [1,-1,0,0,1,-1,1,-1]
+dx = [-1,1,0,0,-1,-1,1,1]
+dy = [0,0,-1,1,1,-1,-1,1]
 for t in range(int(input())):
   n,m = map(int,input().split())
-  d = [[0]*n for _ in range(n)]
-  d[n//2-1][n//2-1] = 2
-  d[n//2-1][n//2] = 1
-  d[n//2][n//2-1] = 1
-  d[n//2][n//2] = 2
+  a = [[0]*n for _ in range(n)]
+  a[n//2-1][n//2-1] = 2
+  a[n//2-1][n//2] = 1
+  a[n//2][n//2-1]=1
+  a[n//2][n//2] = 2
   for _ in range(m):
     x,y,z = map(int,input().split())
     x,y = x-1,y-1
@@ -18,22 +18,23 @@ for t in range(int(input())):
         if not (0 <= nx < n and 0 <= ny < n):
           ans = []
           break
-        if d[nx][ny] == z:
-          break
-        if d[nx][ny] == 0:
+        if a[nx][ny] == 0:
           ans = []
+          break
+        if a[nx][ny] == z:
           break
         else:
           ans.append((nx,ny))
-        nx,ny = nx+dx[k], ny+dy[k]
+        nx += dx[k]
+        ny += dy[k]
       for i,j in ans:
         if z == 1:
-          d[i][j] = 1
+          a[i][j] = 1
         elif z == 2:
-          d[i][j] = 2
-      d[x][y] = z
-  w,b = 0,0
-  for i in range(n):
-    w += d[i].count(2)
-    b += d[i].count(1)
+          a[i][j] = 2
+      a[x][y] = z
+  b,w = 0,0
+  for i in range(len(a[i])):
+    b += a[i].count(1)
+    w += a[i].count(2)
   print(f'#{t+1} {b} {w}')
