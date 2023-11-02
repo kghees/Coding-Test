51aa132ab97f19f93da5db46f0e76588fb68c9ba
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 18:45:19 +0900

    [D3] Title: 격자판 칠하기, Time: 255 ms, Memory: 63,572 KB -BaekjoonHub

--- SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.py
+++ SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.py
@@ -1,38 +1,32 @@
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+def paint(x,y):
+  for k in range(4):
+    nx = x + dx[k]
+    ny = y + dy[k]
+    if 0 <= nx < n and 0 <= ny < m:
+      if a[nx][ny] == '?':
+        if a[x][y] == '#':
+          a[nx][ny] = '.'
+        elif a[x][y] == '.':
+          a[nx][ny] = '#'
+      elif a[x][y] == a[nx][ny]:
+        return False
+  return True
 for t in range(int(input())):
   n,m = map(int,input().split())
   a = [list(input()) for _ in range(n)]
+  check = True
   for i in range(n):
     for j in range(m):
-      if a[i][j] == '#':
-        if (i+j) % 2 == 0:
-          x = '#'
-          y = '.'
-        else:
-          x = '.'
-          y = '#'
-        break
-      elif a[i][j] == '.':
-        if (i+j) % 2 == 0:
-          x = '.'
-          y = '#'
-        else:
-          x = '#'
-          y = '.'
-        break
-  result = 1
-  for i in range(n):
-    for j in range(m):
-      if (i+j) % 2 == 0:
-        if a[i][j] == y:
-          result = 0
-          break
+      if paint(i,j):
+        continue
       else:
-        if a[i][j] == x:
-          result = 0
-          break
-    if result == 0:
+        check = False
+        break
+    if not check:
       break
-  if result == 1:
+  if check:
     print(f'#{t+1} possible')
   else:
     print(f'#{t+1} impossible')
