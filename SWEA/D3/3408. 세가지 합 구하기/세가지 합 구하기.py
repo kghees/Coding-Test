2a9f77330508b3d303f25340ccffb1a7e334251d
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-02 20:10:43 +0900

    [D3] Title: 세가지 합 구하기, Time: 1,224 ms, Memory: 65,148 KB -BaekjoonHub

--- SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
+++ SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
@@ -1,7 +1,7 @@
 for t in range(int(input())):
   n = int(input())
   s1,s2,s3 = 0,0,0
-  s1 = ((1+n)*n)//2
-  s2 += n**2
-  s3 += (s2+n)
+  s1 = (n*(1+n))//2
+  s2 = n**2
+  s3 = s2+n
   print(f'#{t+1} {s1} {s2} {s3}')
