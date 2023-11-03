b72810b878d36661fe054f3de2d0a3542b8c2fc2
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-03 17:00:17 +0900

    [D3] Title: 두 전구, Time: 603 ms, Memory: 67,260 KB -BaekjoonHub

--- SWEA/D3/12741. 두 전구/두 전구.py
+++ SWEA/D3/12741. 두 전구/두 전구.py
@@ -3,9 +3,9 @@ res = []
 for _ in range(t):
   a,b,c,d = map(int,input().split())
   num = [0]*100
-  for i in range(a, b):
+  for i in range(a,b):
     num[i] += 1
-  for i in range(c, d):
+  for i in range(c,d):
     num[i] += 1
   res.append(num.count(2))
 for k in range(t):
