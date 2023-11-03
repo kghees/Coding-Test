db3d1f22921da40601233a2c5f4ca18d1e2d1f84
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-03 18:49:16 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 1,718 ms, Memory: 64,096 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -8,13 +8,15 @@ def check(x):
 for t in range(int(input())):
   n = int(input())
   a = list(map(int,input().split()))
-  res = 0
+  res = []
   for i in range(n-1):
     for j in range(i+1,n):
       x = a[i]*a[j]
       if check(x):
-        if res < x:
-          res = x
-  if res == 0:
-    res = -1
-  print(f'#{t+1} {res}')
+        res.append(x)
+      else:
+        continue
+  if res:
+    print(f'#{t+1} {max(res)}')
+  else:
+    print(f'#{t+1} -1')
