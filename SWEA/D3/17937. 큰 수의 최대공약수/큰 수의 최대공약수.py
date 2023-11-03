586982039a299af5dcf08510d8661c94e8268d6f
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-03 19:10:49 +0900

    [D3] Title: 큰 수의 최대공약수, Time: 144 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D3/17937. 큰 수의 최대공약수/큰 수의 최대공약수.py
+++ SWEA/D3/17937. 큰 수의 최대공약수/큰 수의 최대공약수.py
@@ -1,6 +1,6 @@
 for t in range(int(input())):
   a,b = map(int,input().split())
-  if a < b:
-    print(f'#{t+1} 1')
+  if a == b:
+    print(f'#{t+1} {a}')
   else:
-    print(f'#{t+1} {b}')
+    print(f'#{t+1} 1')
