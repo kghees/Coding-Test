655fffe89d4c5aea9b4e1a371e444269102d6fc5
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-04 23:42:27 +0900

    [Gold V] Title: 토마토, Time: 1336 ms, Memory: 98536 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,27 +1,32 @@
 from collections import deque
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+def bfs():
+  while q:
+    x,y = q.popleft()
+    for k in range(4):
+      nx = x + dx[k]
+      ny = y + dy[k]
+      if (0 <= nx < n and 0 <= ny < m) and a[nx][ny] == 0:
+        a[nx][ny] = a[x][y] + 1
+        q.append((nx,ny))
 m,n = map(int,input().split())
 a = [list(map(int,input().split())) for _ in range(n)]
-arr = [[-1]*m for _ in range(n)]
 q = deque()
 for i in range(n):
   for j in range(m):
     if a[i][j] == 1:
-      arr[i][j] = 0
-      q.append((i,j))
-while q:
-  x,y = q.popleft()
-  for k in range(4):
-    nx = x + dx[k]
-    ny = y + dy[k]
-    if 0 <= nx < n and 0 <= ny < m:
-      if a[nx][ny] == 0 and arr[nx][ny] == -1:
-        arr[nx][ny] = arr[x][y] + 1
-        q.append((nx,ny))
-result = max([max(i) for i in arr])
+      q.append((i,j)) 
+bfs()
+res = max([max(i) for i in a])
 for i in range(n):
   for j in range(m):
-    if a[i][j] == 0 and arr[i][j] == -1:
-      result = -1
-print(result)
+    if a[i][j] == 0:
+      res = -1
+      break
+  if res == -1:
+    break
+if res == -1:
+  print(-1)
+else:
+  print(res-1)
