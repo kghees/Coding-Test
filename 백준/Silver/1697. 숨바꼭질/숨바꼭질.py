b43794cf8781f3d19c88884fac07ade509459197
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-04 22:51:03 +0900

    [Silver I] Title: 숨바꼭질, Time: 164 ms, Memory: 40688 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,17 +1,19 @@
 from collections import deque
+def bfs():
+  q = deque()
+  q.append(n)
+  check[n] = True
+  time[n] = 0
+  while q:
+    x = q.popleft()
+    for nx in [x-1,x+1,x*2]:
+      if 0 <= nx <= max and check[nx] == False:
+        check[nx] = True
+        time[nx] = time[x] + 1
+        q.append(nx)
 n, k = map(int,input().split())
 max = 200000
 time = [-1]*(max+1)
 check = [False]*(max+1)
-check[n] = True
-time[n] = 0
-q = deque()
-q.append(n)
-while q:
-  x = q.popleft()
-  for nx in [x+1,x-1,x*2]:
-    if 0 <= nx <= max and check[nx] == False:
-      check[nx] = True
-      time[nx] = time[x] + 1
-      q.append(nx)
+bfs()
 print(time[k])
