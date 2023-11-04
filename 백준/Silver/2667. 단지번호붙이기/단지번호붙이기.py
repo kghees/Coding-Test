5e4e0f0b9f0e51a896fac8bd06056a0e18f2b72f
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-04 20:05:30 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31332 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,32 +1,31 @@
-from collections import deque
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-def bfs(d,a,b):
-  n = len(d)
-  q = deque()
-  q.append((a,b))
-  d[a][b] = 0
-  count = 1
-  while q:
-    x,y = q.popleft()
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if nx < 0 or nx >= n or ny < 0 or ny >= n:
-        continue
-      if d[nx][ny] == 1:
-        d[nx][ny] = 0
-        q.append((nx,ny))
-        count += 1
-  return count
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+def dfs(x,y):
+  global cnt
+  if x < 0 or x >= n or y < 0 or y >= n:
+    return False
+  if a[x][y] == 1:
+    cnt += 1
+    a[x][y] = 0
+    for k in range(4):
+      nx = x + dx[k]
+      ny = y + dy[k]
+      dfs(nx,ny)
+    return True
+  return False
 n = int(input())
-d = [list(map(int,input())) for _ in range(n)]
-cnt = []
+a = [list(map(int,input())) for _ in range(n)]
+cnt = 0
+result = 0
+arr = []
 for i in range(n):
   for j in range(n):
-    if d[i][j] == 1:
-      cnt.append(bfs(d,i,j))
-cnt.sort()
-print(len(cnt))
-for i in cnt:
+    if a[i][j] == 1:
+      if dfs(i,j):
+        arr.append(cnt)
+        result += 1
+        cnt = 0
+arr.sort()
+print(result)
+for i in arr:
   print(i)
