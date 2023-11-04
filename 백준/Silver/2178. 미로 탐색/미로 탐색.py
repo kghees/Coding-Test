09156de42cc22da32051f61e4fe748596134e629
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-04 19:08:19 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,6 +1,6 @@
 from collections import deque
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
 def bfs(x,y):
   q = deque()
   q.append((x,y))
@@ -9,14 +9,14 @@ def bfs(x,y):
     for k in range(4):
       nx = x + dx[k]
       ny = y + dy[k]
-      if nx < 0 or nx >= n or ny < 0 or ny >= m:
+      if not (0 <= nx < n and 0 <= ny < m):
         continue
-      if d[nx][ny] == 0:
+      if a[nx][ny] == 0:
         continue
-      if d[nx][ny] == 1:
-        d[nx][ny] = d[x][y] + 1
+      if a[nx][ny] == 1:
+        a[nx][ny] = a[x][y] + 1
         q.append((nx,ny))
-  return d[n-1][m-1]
 n,m = map(int,input().split())
-d = [list(map(int,input())) for _ in range(n)]
-print(bfs(0,0))
+a = [list(map(int,input())) for _ in range(n)]
+bfs(0,0)
+print(a[n-1][m-1])
