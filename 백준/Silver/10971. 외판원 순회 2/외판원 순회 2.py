a1a00c234f6725af003f7bbeed93d4ace50eb61c
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-05 22:56:29 +0900

    [Silver II] Title: 외판원 순회 2, Time: 140 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,37 +1,24 @@
-def next_permutation(a):
-  i = len(a) - 1
-  while i > 0 and a[i] <= a[i-1]:
-    i -= 1
-  if i <= 0:
-    return False
-  j = len(a) - 1
-  while a[j] <= a[i-1]:
-    j -= 1
-  a[i-1],a[j] = a[j],a[i-1]
-  j = len(a) - 1
-  while i < j:
-    a[i],a[j] = a[j],a[i]
-    i += 1
-    j -= 1
-  return True
+def dfs(start,next,ans,cnt):
+  global res
+  if cnt == n:
+    if a[next][start]:
+      ans += a[next][start]
+      if res > ans:
+        res = ans
+    return
+  if ans > res:
+    return
+  for i in range(n):
+    if not check[i] and a[next][i]:
+      check[i] = True
+      dfs(start,i,ans+a[next][i],cnt+1)
+      check[i] = False
 n = int(input())
 a = [list(map(int,input().split())) for _ in range(n)]
-d = [i for i in range(n)]
-ans = 2147483647
-while True:
-  ok = True
-  num = 0
-  for i in range(0, n-1):
-    if a[d[i]][d[i+1]] == 0:
-      ok = False
-      break
-    else:
-      num += a[d[i]][d[i+1]]
-  if ok and a[d[-1]][d[0]] != 0:
-    num += a[d[-1]][d[0]]
-    ans = min(ans,num)
-  if not next_permutation(d):
-    break
-  if d[0] != 0:
-    break
-print(ans)
+res = 1e9
+check = [False]*n
+for i in range(n):
+  check[i] = True
+  dfs(i,i,0,1)
+  check[i] = False
+print(res)
