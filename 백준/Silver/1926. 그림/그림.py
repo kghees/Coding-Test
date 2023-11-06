9781aa985b1f89db866b36108a9facfb96eb81b2
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-06 21:07:51 +0900

    [Silver I] Title: 그림, Time: 344 ms, Memory: 34888 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,32 +1,30 @@
-import sys
-sys.setrecursionlimit(1000000)
+from collections import deque
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
-def dfs(x,y):
-  global cnt
-  if not (0 <= x < n and 0 <= y < m):
-    return False
-  if a[x][y] == 1:
-    a[x][y] = 0
-    cnt += 1
+def bfs(x,y):
+  q = deque()
+  q.append((x,y))
+  cnt = 1
+  a[x][y] = 0
+  while q:
+    x,y = q.popleft()
     for k in range(4):
       nx = x + dx[k]
       ny = y + dy[k]
-      dfs(nx,ny)
-    return True
-  return False
+      if (0 <= nx < n and 0 <= ny < m) and a[nx][ny] == 1:
+        a[nx][ny] = 0
+        cnt += 1
+        q.append((nx,ny))
+  return cnt
 n,m = map(int,input().split())
 a = [list(map(int,input().split())) for _ in range(n)]
 res = 0
-cnt = 0
 arr = []
 for i in range(n):
   for j in range(m):
     if a[i][j] == 1:
-      if dfs(i,j):
-        res += 1
-        arr.append(cnt)
-        cnt = 0
+      res += 1
+      arr.append(bfs(i,j))
 x = 0
 if arr:
   x = max(arr)
