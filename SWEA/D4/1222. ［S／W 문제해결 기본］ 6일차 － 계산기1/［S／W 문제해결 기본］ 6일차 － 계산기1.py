ce8c596a0be1c6c039017898e29524795536bc32
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-07 11:39:33 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 151 ms, Memory: 58,248 KB -BaekjoonHub

---
+++ SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.py
@@ -0,0 +1,21 @@
+for t in range(1, 11):
+  n = int(input())
+  a = list(input())
+  res = ''
+  stack = []
+  for i in a:
+    if i == '+':
+      while stack:
+        res += stack.pop()
+      stack.append(i)
+    else:
+      res += i
+  while stack:
+    res += stack.pop()
+  stack = []
+  for j in res:
+    if j == '+':
+      stack.append(stack.pop()+stack.pop())
+    else:
+      stack.append(int(j))
+  print(f'#{t} {stack[-1]}')
