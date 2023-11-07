2d9d310f00b349e3241dc241bcf5dabe37b62710
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-07 22:41:29 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 156 ms, Memory: 58,544 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
@@ -1,45 +1,42 @@
 for t in range(1,11):
   n = int(input())
   a = list(input())
-  stack1 = []
-  stack2 = []
-  stack3 = []
-  stack4 = []
+  stack = []
   check = True
   for i in a:
     if i == '(':
-      stack1.append('(')
+      stack.append('(')
     elif i == ')':
-      if stack1 and stack1[-1] == '(':
-        stack1.pop()
+      if stack and stack[-1] == '(':
+        stack.pop()
       else:
         check = False
         break
     elif i == '[':
-      stack2.append('[')
+      stack.append('[')
     elif i == ']':
-      if stack2 and stack2[-1] == '[':
-        stack2.pop()
+      if stack and stack[-1] == '[':
+        stack.pop()
       else:
         check = False
         break
     elif i == '{':
-      stack3.append('{')
+      stack.append('{')
     elif i == '}':
-      if stack3 and stack3[-1] == '{':
-        stack3.pop()
+      if stack and stack[-1] == '{':
+        stack.pop()
       else:
         check = False
         break
     elif i == '<':
-      stack4.append('<')
+      stack.append('<')
     elif i == '>':
-      if stack4 and stack4[-1] == '<':
-        stack4.pop()
+      if stack and stack[-1] == '<':
+        stack.pop()
       else:
         check = False
         break
-  if check and not stack1 and not stack2 and not stack3 and not stack4:
+  if check and not stack:
     print(f'#{t} 1')
   else:
     print(f'#{t} 0')
