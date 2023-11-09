41f0e92bf3ae7ad17c27c16a7d076c58fe4e4cc2
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-09 16:09:38 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 159 ms, Memory: 59,716 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -1,25 +1,17 @@
-dx = [1,0,0]
-dy = [0,1,-1]
-for _ in range(1,11):
+for _ in range(10):
   t = int(input())
   a = [list(map(int,input().split())) for _ in range(100)]
-  res = 0
-  for i in range(100):
-    if a[0][i] == 1:
-      x,y = 0,i
-      d = 0
-      while x < 99:
-        if d == 0:
-          if y > 0 and a[x][y-1] == 1:
-            d = 2
-          elif y < 99 and a[x][y+1] == 1:
-            d = 1
-        else:
-          if a[x+1][y] == 1:
-            d = 0
-        x += dx[d]
-        y += dy[d]
-        if a[x][y] == 2:
-          res = i
-          break
-  print(f'#{t} {res}')
+  x = 99
+  y = a[99].index(2)
+  while x != 0:
+    if 0 <= y+1 < 100 and a[x][y+1] == 1:
+      while y+1 < 100 and a[x][y+1] == 1:
+        y += 1
+      x -= 1
+    elif 0 <= y-1 < 100 and a[x][y-1] == 1:
+      while y-1 >= 0 and a[x][y-1] == 1:
+        y -= 1
+      x -= 1
+    else:
+      x -= 1
+  print(f'#{t} {y}')
