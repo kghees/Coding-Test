30f9768c009d033b7cd7053f302fe998bdeb0319
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-09 16:55:48 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 192 ms, Memory: 59,180 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -4,12 +4,12 @@ for _ in range(10):
   x = 99
   y = a[99].index(2)
   while x != 0:
-    if 0 <= y+1 < 100 and a[x][y+1] == 1:
-      while y+1 < 100 and a[x][y+1] == 1:
+    if 0 <= y < 99 and a[x][y+1] == 1:
+      while y < 99 and a[x][y+1] == 1:
         y += 1
       x -= 1
-    elif 0 <= y-1 < 100 and a[x][y-1] == 1:
-      while y-1 >= 0 and a[x][y-1] == 1:
+    elif 0 < y < 100 and a[x][y-1] == 1:
+      while y > 0 and a[x][y-1] == 1:
         y -= 1
       x -= 1
     else:
