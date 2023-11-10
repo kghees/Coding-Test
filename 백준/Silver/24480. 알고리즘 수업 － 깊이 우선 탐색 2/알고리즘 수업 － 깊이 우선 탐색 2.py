2aa26dd873252cb865bccbfee46f406bd23c4870
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-10 14:53:14 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 640 ms, Memory: 67996 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,23 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+def dfs(x):
+  global cnt
+  check[x] = cnt
+  for i in a[x]:
+    if not check[i]:
+      cnt += 1
+      dfs(i)
+n,m,r = map(int,input().split())
+a = [[] for _ in range(n+1)]
+for _ in range(m):
+  u,v = map(int,input().split())
+  a[u].append(v)
+  a[v].append(u)
+check = [0]*(n+1)
+cnt = 1
+for i in range(n+1):
+  a[i].sort(reverse=True)
+dfs(r)
+for i in range(1, n+1):
+  print(check[i])
