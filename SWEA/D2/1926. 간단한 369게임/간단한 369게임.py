e5a8224b82377292846df88c8fc1e814e32bee60
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-16 18:10:27 +0900

    [D2] Title: 간단한 369게임, Time: 97 ms, Memory: 42,460 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -1,8 +1,10 @@
 n = int(input())
+res = []
 for i in range(1, n+1):
   x = str(i)
-  cnt = x.count('3') + x.count('6') + x.count('9')
-  if cnt != 0:
-    print('-'*cnt,end=' ')
+  a = x.count('3') + x.count('6') + x.count('9')
+  if a == 0:
+    res.append(i)
   else:
-    print(x,end=' ')
+    res.append('-'*a)
+print(*res)
