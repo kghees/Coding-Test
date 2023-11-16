24a51f81ce414ffe1ed78d6e4112d52de21b4d93
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-16 17:05:40 +0900

    [D2] Title: Base64 Decoder, Time: 103 ms, Memory: 42,972 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
@@ -1,14 +1,14 @@
+decode = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 for t in range(int(input())):
-  d ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
   s = input()
   ans = ''
-  arr = []
-  result = ''
+  res = ''
+  a = []
   for i in s:
-    num = d.index(i)
-    ans += bin(num)[2:].zfill(6)
-  for i in range(0, len(ans)-7, 8):
-    arr.append(ans[i:i+8])
-  for i in arr:
-    result += chr(int(i,2))
-  print(f'#{t+1} {result}')
+    x = decode.index(i)
+    ans += bin(x)[2:].zfill(6)
+  for i in range(0,len(ans)-7,8):
+    a.append(ans[i:i+8])
+  for i in a:
+    res += chr(int(i,2))
+  print(f'#{t+1} {res}')
