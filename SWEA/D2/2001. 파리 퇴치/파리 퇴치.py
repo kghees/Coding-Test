efcf04da8aa67303b22e7e06c518b26b334ab1e9
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-16 16:47:49 +0900

    [D2] Title: 파리 퇴치, Time: 129 ms, Memory: 47,216 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,13 +1,13 @@
 for t in range(int(input())):
   n,m = map(int,input().split())
   a = [list(map(int,input().split())) for _ in range(n)]
-  result = 0
+  res = 0
   for i in range(n-m+1):
     for j in range(n-m+1):
       ans = 0
       for k in range(m):
         for l in range(m):
           ans += a[i+k][j+l]
-      if ans > result:
-        result = ans
-  print(f'#{t+1} {result}')
+      if res < ans:
+        res = ans
+  print(f'#{t+1} {res}')
