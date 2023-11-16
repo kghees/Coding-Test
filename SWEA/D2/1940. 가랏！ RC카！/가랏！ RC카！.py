637f43ae89fc68f36a6915a3a31712308a85a820
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-16 22:52:26 +0900

    [D2] Title: 가랏! RC카!, Time: 100 ms, Memory: 42,724 KB -BaekjoonHub

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -1,15 +1,15 @@
 for t in range(int(input())):
   n = int(input())
-  result = 0
-  cnt = 0
-  for i in range(n):
+  res = 0
+  x = 0
+  for _ in range(n):
     a = list(map(int,input().split()))
     if a[0] == 1:
-      cnt += a[1]
+      x += a[1]
     elif a[0] == 2:
-      if cnt < a[1]:
-        cnt = 0
+      if x - a[1] < 0:
+        x = 0
       else:
-        cnt -= a[1]
-    result += cnt
-  print(f'#{t+1} {result}')
+        x -= a[1]
+    res += x
+  print(f'#{t+1} {res}')
