3083a93ae8f8f6be8e5f952f05fd2093eee51695
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-16 21:55:35 +0900

    [D2] Title: 날짜 계산기, Time: 96 ms, Memory: 42,440 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
@@ -1,5 +1,5 @@
 for t in range(int(input())):
   a,b,c,d = map(int,input().split())
-  num = [0,31,59,90,120,151,181,212,243,273,304,334,365]
-  result = (num[c-1]+d) - (num[a-1]+b) + 1
-  print(f'#{t+1} {result}')
+  day = [0,31,59,90,120,151,181,212,243,273,304,334,365]
+  res = (d+day[c-1]) - (b+day[a-1]) + 1
+  print(f'#{t+1} {res}')
