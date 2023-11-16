cb52d4ad2fc21c00d6267b460919cff903dfb0e0
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-16 22:34:29 +0900

    [D2] Title: 중간 평균값 구하기, Time: 95 ms, Memory: 42,996 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.py
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.py
@@ -1,5 +1,5 @@
 for t in range(int(input())):
   a = list(map(int,input().split()))
   a.sort()
-  avg = sum(a[1:9]) / 8
-  print(f'#{t+1} {round(avg)}')
+  avg = round(sum(a[1:9])/8)
+  print(f'#{t+1} {avg}')
