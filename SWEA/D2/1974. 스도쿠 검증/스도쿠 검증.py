c1881da33c89bb5aae4ad23be448149c5703a468
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-16 18:55:14 +0900

    [D2] Title: 스도쿠 검증, Time: 101 ms, Memory: 45,572 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,23 +1,27 @@
-def sdoku(a):
-  for i in range(9):
-    cnt1 = [0]*10
-    cnt2 = [0]*10
-    for j in range(9):
-      cnt1[a[i][j]] += 1
-      if cnt1[a[i][j]] >= 2:
-        return 0
-      cnt2[a[j][i]] += 1
-      if cnt2[a[j][i]] >= 2:
-        return 0
-  for k in range(0,9,3):
-    for l in range(0,9,3):
-      cnt3 = [0]*10
-      for x in range(3):
-        for y in range(3):
-          cnt3[a[k+x][l+y]] += 1
-          if cnt3[a[k+x][l+y]] >= 2:
-            return 0
-  return 1
 for t in range(int(input())):
   a = [list(map(int,input().split())) for _ in range(9)]
-  print(f'#{t+1} {sdoku(a)}')
+  res = 1
+  for i in range(9):
+    num = [0]*(10)
+    num1 = [0]*(10)
+    for j in range(9):
+      num[a[i][j]] += 1
+      num1[a[j][i]] += 1
+    if num[1:].count(1) != 9:
+      res = 0
+      break
+    if num1[1:].count(1) != 9:
+      res = 0
+      break
+  for i in range(0,9,3):
+    for j in range(0,9,3):
+      num2 = [0]*10
+      for k in range(3):
+        for l in range(3):
+          num2[a[i+k][j+l]] += 1
+      if num2[1:].count(1) != 9:
+        res = 0
+        break
+    if not res:
+      break
+  print(f'#{t+1} {res}')
