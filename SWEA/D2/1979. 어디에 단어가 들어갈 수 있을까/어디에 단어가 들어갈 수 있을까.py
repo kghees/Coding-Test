43351880cc43163377b72dd4d9cf15884f0c7b2d
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-16 18:36:06 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 106 ms, Memory: 48,752 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,23 +1,24 @@
 for t in range(int(input())):
   n,k = map(int,input().split())
   a = [list(map(int,input().split())) for _ in range(n)]
-  cnt = 0
+  res = 0
   for i in range(n):
-    x = 0
-    y = 0
+    x,y = 0,0
     for j in range(n):
       if a[i][j] == 1:
         x += 1
-      if a[i][j] == 0 or j == n-1:
+      elif a[i][j] == 0:
         if x == k:
-          cnt += 1
-        if a[i][j] == 0:
-          x = 0
+          res += 1
+        x = 0
       if a[j][i] == 1:
         y += 1
-      if a[j][i] == 0 or j == n-1:
+      elif a[j][i] == 0:
         if y == k:
-          cnt += 1
-        if a[j][i] == 0:
-          y = 0
-  print(f'#{t+1} {cnt}')
+          res += 1
+        y = 0
+    if x == k:
+      res += 1
+    if y == k:
+      res += 1
+  print(f'#{t+1} {res}')
