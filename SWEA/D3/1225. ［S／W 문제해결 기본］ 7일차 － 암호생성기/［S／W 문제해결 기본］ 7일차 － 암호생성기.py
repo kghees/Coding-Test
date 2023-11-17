09307dea4a0328edd835c34405c1337034f6a0de
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-17 17:14:17 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 120 ms, Memory: 52,100 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,14 +1,14 @@
-for t in range(1,11):
+for t in range(10):
   n = int(input())
   a = list(map(int,input().split()))
   i = 1
   while True:
+    a[0] -= i
+    a.append(a.pop(0))
     if a[-1] <= 0:
       a[-1] = 0
       break
-    a[0] -= i
     i += 1
-    a.append(a.pop(0))
     if i > 5:
       i = 1
-  print(f'#{t}',*a)
+  print(f'#{t+1}',*a)
