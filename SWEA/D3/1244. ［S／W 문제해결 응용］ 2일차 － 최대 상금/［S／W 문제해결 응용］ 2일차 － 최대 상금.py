9b51167e6bcb3493c83709b79f173ceeb6fdd9c2
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-17 14:47:51 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 199 ms, Memory: 61,232 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,4 +1,4 @@
-def money(x,cnt):
+def dfs(x,cnt):
   global res
   if cnt == int(n):
     res = max(int(''.join(a)),res)
@@ -7,16 +7,16 @@ def money(x,cnt):
     for j in range(i+1,len(a)):
       if a[i] <= a[j]:
         a[i],a[j] = a[j],a[i]
-        money(i,cnt+1)
+        dfs(i,cnt+1)
         a[i],a[j] = a[j],a[i]
   if res == 0 and int(n) > cnt:
-    k = (int(n) - cnt) % 2
+    k = (int(n)-cnt) % 2 
     if k == 1:
       a[-1],a[-2] = a[-2],a[-1]
-    money(x,int(n))
+    dfs(x,int(n))
 for t in range(int(input())):
-  a, n = input().split()
+  a,n = input().split()
   a = list(a)
   res = 0
-  money(0,0)
+  dfs(0,0)
   print(f'#{t+1} {res}')
