c0cb584bf184d6d22dae87972c44fdb7ca80cbd4
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-18 17:43:06 +0900

    [D4] Title: 정사각형 방, Time: 956 ms, Memory: 61,552 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -2,7 +2,7 @@ from collections import deque
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 def bfs(x,y):
-  global cnt
+  global z
   q = deque()
   q.append((x,y))
   while q:
@@ -12,21 +12,22 @@ def bfs(x,y):
       ny = y + dy[k]
       if 0 <= nx < n and 0 <= ny < n:
         if a[x][y] + 1 == a[nx][ny]:
-          cnt += 1
+          z += 1
           q.append((nx,ny))
-  return cnt
+  return z
 for t in range(int(input())):
   n = int(input())
   a = [list(map(int,input().split())) for _ in range(n)]
-  ans = [1e9,0]
+  cnt = 1
+  index = 1e9
   for i in range(n):
     for j in range(n):
-      cnt = 1
-      x = bfs(i,j)
-      if x > ans[1]:
-        ans[1] = x
-        ans[0] = a[i][j]
-      elif x == ans[1]:
-        if ans[0] > a[i][j]:
-          ans[0] = a[i][j]
-  print(f'#{t+1}',*ans)
+      z = 1
+      z = bfs(i,j)
+      if cnt < z:
+        cnt = z
+        index = a[i][j]
+      elif cnt == z:
+        if index > a[i][j]:
+          index = a[i][j]
+  print(f'#{t+1} {index} {cnt}')
