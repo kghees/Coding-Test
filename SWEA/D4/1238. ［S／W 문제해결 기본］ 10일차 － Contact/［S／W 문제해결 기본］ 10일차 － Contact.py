652915e5b06fcfa03209dbc5cfdb8c4bb818ad0a
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-18 18:13:46 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 132 ms, Memory: 51,436 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
@@ -3,26 +3,27 @@ def bfs(x):
   q = deque()
   q.append(x)
   check[x] = True
+  num[x] = 1
   while q:
     x = q.popleft()
     for i in d[x]:
       if not check[i]:
         check[i] = True
-        num[i] = num[x] + 1
+        num[i] = num[x]+1
         q.append(i)
-for t in range(1,11):
-  n,start = map(int,input().split())
+for t in range(10):
+  n, m = map(int,input().split())
   a = list(map(int,input().split()))
   d = [[] for _ in range(101)]
-  for i in range(0,n,2):
+  for i in range(0,len(a),2):
     d[a[i]].append(a[i+1])
-  check = [False]*(101)
-  num = [0]*(101)
-  bfs(start)
+  check = [False]*101
+  num = [0]*101
+  bfs(m)
   res = 0
   k = 0
-  for i in range(1, 101):
+  for i in range(101):
     if k <= num[i]:
       k = num[i]
       res = i
-  print(f'#{t} {res}')
+  print(f'#{t+1} {res}')
