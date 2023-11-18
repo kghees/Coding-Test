12dfcd03f309ed3dff26cb4867ee0a6cef7f42f2
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-18 14:59:19 +0900

    [D4] Title: 가능한 시험 점수, Time: 170 ms, Memory: 59,892 KB -BaekjoonHub

--- SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.py
+++ SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.py
@@ -1,11 +1,11 @@
 for t in range(int(input())):
   n = int(input())
   a = list(map(int,input().split()))
-  res = [1] + [0]*sum(a)
-  ans = [0]
+  ans = [1]+[0]*sum(a)
+  res = [0]
   for i in a:
-    for j in range(len(ans)):
-      if not res[i+ans[j]]:
-        res[i+ans[j]] = 1
-        ans.append(i+ans[j])
-  print(f'#{t+1} {len(ans)}')
+    for j in range(len(res)):
+      if not ans[i+res[j]]:
+        ans[i+res[j]] = 1
+        res.append(i+res[j])
+  print(f'#{t+1} {len(res)}')
