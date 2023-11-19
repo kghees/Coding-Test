e08b7bc429806f1a04ce97e6288ee0cd3973c273
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-19 14:51:17 +0900

    [D3] Title: 새샘이의 7-3-5 게임, Time: 1,624 ms, Memory: 63,892 KB -BaekjoonHub

--- SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.py
+++ SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.py
@@ -1,11 +1,11 @@
+from itertools import combinations
 for t in range(int(input())):
   a = list(map(int,input().split()))
+  num = list(combinations(a,3))
+  num.sort()
   res = []
-  for i in range(len(a)):
-    for j in range(i+1,len(a)):
-      for k in range(j+1,len(a)):
-        x = a[i]+a[j]+a[k]
-        if x not in res:
-          res.append(x)
+  for i in range(len(num)):
+    res.append(sum(num[i]))
+  res = list(set(res))
   res.sort(reverse=True)
   print(f'#{t+1} {res[4]}')
