9debe68b71edf594ab24e66bf686580db6c496b4
김건희 <92205960+kghees@users.noreply.github.com>
2023-12-28 15:41:46 +0900

    [D2] Title: 스도쿠 검증, Time: 101 ms, Memory: 45,572 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,27 +1,23 @@
+def check(a):
+  for i in range(9):
+    row = [0]*10
+    col = [0]*10
+    for j in range(9):
+      row[a[i][j]] += 1
+      if row[a[i][j]] >= 2:
+        return 0
+      col[a[j][i]] += 1
+      if col[a[j][i]] >= 2:
+        return 0
+    for k in range(0,9,3):
+      for l in range(0,9,3):
+        square = [0]*10
+        for x in range(3):
+          for y in range(3):
+            square[a[k+x][l+y]] += 1
+            if square[a[k+x][l+y]] >= 2:
+              return 0
+  return 1
 for t in range(int(input())):
   a = [list(map(int,input().split())) for _ in range(9)]
-  res = 1
-  for i in range(9):
-    num = [0]*(10)
-    num1 = [0]*(10)
-    for j in range(9):
-      num[a[i][j]] += 1
-      num1[a[j][i]] += 1
-    if num[1:].count(1) != 9:
-      res = 0
-      break
-    if num1[1:].count(1) != 9:
-      res = 0
-      break
-  for i in range(0,9,3):
-    for j in range(0,9,3):
-      num2 = [0]*10
-      for k in range(3):
-        for l in range(3):
-          num2[a[i+k][j+l]] += 1
-      if num2[1:].count(1) != 9:
-        res = 0
-        break
-    if not res:
-      break
-  print(f'#{t+1} {res}')
+  print(f'#{t+1} {check(a)}')
