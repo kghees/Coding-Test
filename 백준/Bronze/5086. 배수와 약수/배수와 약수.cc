fec7fb80f65d301c9c54fde5389ab4c13d8d8772
김건희 <92205960+kghees@users.noreply.github.com>
2024-01-20 19:01:15 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a, b;
+    while (true) {
+        cin >> a >> b;
+        if (a == 0 && b == 0)
+            break;
+        if (b % a == 0)
+            cout << "factor" << endl;
+        else if (a % b == 0)
+            cout << "multiple" << endl;
+        else
+            cout << "neither" << endl;
+    }
+    return 0;
+}
