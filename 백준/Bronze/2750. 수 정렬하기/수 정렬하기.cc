40faeb3db2c6b27a1009f9db3ca557e4a4a888d2
김건희 <92205960+kghees@users.noreply.github.com>
2024-01-21 22:03:22 +0900

    [Bronze II] Title: 수 정렬하기, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+    return 0;
+}
