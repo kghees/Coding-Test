3583c188dab7dd3df89b8d4fc72b78a3c79a50e6
김건희 <92205960+kghees@users.noreply.github.com>
2024-01-21 16:22:36 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    cout << n*n*n << endl << 3;
+    return 0;
+}
