e8af1da38c72d69bbb369a8b862df5f7807756dc
김건희 <92205960+kghees@users.noreply.github.com>
2024-02-07 20:25:26 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+using namespace std;
+int t, m, n,k;
+int cabbage[51][51];
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { -1,1,0,0 };
+
+void bfs(int x, int y) {
+    queue<pair<int, int>> q;
+    q.push({ x,y });
+    cabbage[x][y] = 0;
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int l = 0; l < 4; l++) {
+            int nx = x + dx[l];
+            int ny = y + dy[l];
+            if (nx < m && nx >= 0 && ny < n && ny >= 0) {
+                if (cabbage[nx][ny] == 1) {
+                    cabbage[nx][ny] = 0;
+                    q.push({ nx,ny });
+                }
+            }
+        }
+    }
+}
+
+void init() {
+    memset(cabbage, 0, sizeof(cabbage));
+}
+
+int main() {
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> m >> n >> k;
+        init();
+        for (int i = 0; i < k; i++) {
+            int a, b;
+            cin >> a >> b;
+            cabbage[a][b] = 1;
+        }
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (cabbage[i][j] == 1) {
+                    bfs(i, j);
+                    res++;
+                }
+            }
+        }
+        cout << res << '\n';
+    }
+    return 0;
+}
