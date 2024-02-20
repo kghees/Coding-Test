d2808bf3b868030a9cb4c96a69b91b024b6b7de0
김건희 <92205960+kghees@users.noreply.github.com>
2024-02-20 22:22:49 +0900

    [Silver IV] Title: 주몽, Time: 84 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -3,24 +3,15 @@ using namespace std;
 int n, m;
 int arr[15001];
 int res = 0;
-int cnt = 0;
-void dfs(int x,int y) {
-    if (y == 2) {
-        if (res == m)
-            cnt++;
-        return;
-    }
-    for (int i = x+1; i < n; i++) {
-        res += arr[i];
-        dfs(i,y+1);
-        res -= arr[i];
-    }
-}
 int main() {
     cin >> n >> m;
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    dfs(-1,0);
-    cout << cnt;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == m) res++;
+        }
+    }
+    cout << res;
     return 0;
 }
