a5092cc65097adc14668825478de544cd0ea300f
김건희 <92205960+kghees@users.noreply.github.com>
2024-03-05 17:39:26 +0900

    [Silver II] Title: 섬의 개수, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.cc
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 #include <cstring>
 using namespace std;
 
@@ -8,25 +7,17 @@ int map[51][51];
 int dy[8] = {0,0,-1,1,-1,-1,1,1};
 int dx[8] = {-1,1,0,0,-1,1,-1,1};
 
-void bfs(int y, int x) {
-    queue<pair<int, int>> q;
-    q.push({ y,x });
+void dfs(int y, int x) {
     map[y][x] = 0;
 
-    while (!q.empty()) {
-        int y = q.front().first;
-        int x = q.front().second;
-        q.pop();
+    for (int i = 0; i < 8; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
 
-        for (int i = 0; i < 8; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-
-            if (ny < h && ny >= 0 && nx < w && nx >= 0) {
-                if (map[ny][nx] == 1) {
-                    map[ny][nx] = 0;
-                    q.push({ ny,nx });
-                }
+        if (ny < h && ny >= 0 && nx < w && nx >= 0) {
+            if (map[ny][nx] == 1) {
+                map[ny][nx] = 0;
+                dfs(ny, nx);
             }
         }
     }
@@ -42,16 +33,15 @@ int main(){
                 cin >> map[i][j];
             }
         }
-        int res = 0;
+        int res = 0; //섬의 개수
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
                 if (map[i][j] == 1) {
-                    bfs(i, j);
+                    dfs(i, j); //1일때 마다 돌고 모두 1인거 모두 0으로 처리해주면 최종 섬의 개수 나옴
                     res++;
                 }
             }
         }
         cout << res << '\n';
     }
-    
 }
