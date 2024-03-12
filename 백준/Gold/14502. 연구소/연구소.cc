385e3ad7de5a69c996ffe99cce53a5e0a1c57dfd
김건희 <92205960+kghees@users.noreply.github.com>
2024-03-12 20:35:36 +0900

    [Gold IV] Title: 연구소, Time: 56 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,38 +1,23 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
+#include <vector>
 #include <cstring>
+#include <queue>
 using namespace std;
 
 int n, m;
-int map[8][8];
+int a[8][8];
+vector<pair<int,int>> virusList;
+vector<pair<int,int>> wallList;
+int ret = 0;
+int visited[8][8];
 int dy[4] = { 0,0,-1,1 };
 int dx[4] = { -1,1,0,0 };
-int res = 0;
-int copymap[8][8];
-bool visit[8][8] = { false };
 
-void copy(int copymap[8][8], int map[8][8]) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            copymap[i][j] = map[i][j];
-        }
-    }
-
-}
-
-
-void bfs() {
-    int check[8][8];
-    copy(check, copymap);
+void bfs(int y, int x) {
     queue<pair<int, int>> q;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (check[i][j] == 2) {
-                q.push({ i,j });
-            }
-        }
-    }
+    q.push({ y,x });
+
     while (!q.empty()) {
         int y = q.front().first;
         int x = q.front().second;
@@ -41,61 +26,52 @@ void bfs() {
         for (int k = 0; k < 4; k++) {
             int ny = y + dy[k];
             int nx = x + dx[k];
+
             if (ny >= n || ny < 0 || nx >= m || nx < 0) continue;
-            if (check[ny][nx] == 0) {
-                check[ny][nx] = 2;
-                q.push({ ny,nx });
-            }
+            if (visited[ny][nx] || a[ny][nx] == 1) continue;
+            visited[ny][nx] = 1;
+            q.push({ ny,nx });
         }
     }
-    int temp = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (check[i][j] == 0) temp++;
-        }
-    }
-    res = max(res, temp);
+    return;
 }
 
-void dfs(int cnt) {
-    if (cnt == 3) {
-        bfs();
-        return;
+int solve() {
+    memset(visited, 0, sizeof(visited));
+    for (pair<int, int> i : virusList) {
+        visited[i.first][i.second] = 1;
+        bfs(i.first, i.second);
     }
+    int cnt = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (copymap[i][j] == 0) {
-                copymap[i][j] = 1;
-                dfs(cnt+1);
-                copymap[i][j] = 0;
-            }
+            if (a[i][j] == 0 && !visited[i][j]) cnt++;
         }
     }
-
+    return cnt;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cin >> map[i][j];
+            cin >> a[i][j];
+            if (a[i][j] == 2) virusList.push_back({ i,j });
+            if (a[i][j] == 0) wallList.push_back({ i,j });
         }
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (map[i][j] == 0) {
-                memset(visit, false, sizeof(visit));
-                copy(copymap, map);
-                copymap[i][j] = 1;
-                dfs(1);
-                copymap[i][j] = 0;
+    for (int i = 0; i < wallList.size(); i++) {
+        for (int j = 0; j < i; j++) {
+            for (int k = 0; k < j; k++) {
+                a[wallList[i].first][wallList[i].second] = 1;
+                a[wallList[j].first][wallList[j].second] = 1;
+                a[wallList[k].first][wallList[k].second] = 1;
+                ret = max(ret, solve());
+                a[wallList[i].first][wallList[i].second] = 0;
+                a[wallList[j].first][wallList[j].second] = 0;
+                a[wallList[k].first][wallList[k].second] = 0;
             }
         }
     }
-    cout << res;
-    return 0;
+    cout << ret;
 }
-
