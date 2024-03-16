4308c432f28b78592be2aa6c2518c4e4c3cd0ce0
김건희 <92205960+kghees@users.noreply.github.com>
2024-03-16 17:26:19 +0900

    [Gold IV] Title: 오큰수, Time: 600 ms, Memory: 15988 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,36 +1,26 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cstring>
 using namespace std;
 
 
 int n;
-vector<int> arr;
+int arr[1000001];
 int dat[1000001];
 vector<int> st;
 int main() {
 	cin >> n;
+	memset(dat, -1, sizeof(dat));
 	for (int i = 0; i < n; i++) {
-		int a;
-		cin >> a;
-		arr.push_back(a);
-	}
-	st.push_back(0);
-	for (int i = 1; i < n; i++) {
-		if (st.empty()) st.push_back(i);
-
-		while (!st.empty() && arr[st.back()] < arr[i]) {
+		cin >> arr[i];
+		while (st.size() && arr[st.back()] < arr[i]) {
 			dat[st.back()] = arr[i];
 			st.pop_back();
 		}
 		st.push_back(i);
 	}
-	while (!st.empty()) {
-		dat[st.back()] = -1;
-		st.pop_back();
-	}
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
 		cout << dat[i] << " ";
-	}
 	return 0;
 }
