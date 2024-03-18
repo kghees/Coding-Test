e64c49b0657a67741da87cdd652f72f85beea9ac
김건희 <92205960+kghees@users.noreply.github.com>
2024-03-18 20:30:22 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,58 +1,67 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <cstring>
 using namespace std;
 
 int n, m;
 int map[51][51];
+int res = 1e9;
+vector<vector<int>> chickenList;
 vector<pair<int, int>> home;
 vector<pair<int, int>> chicken;
-int res = 1e9;
-bool visited[13] = { false };
 
-void calc() {
-    int temp[51][51];
-    memset(temp, 10000, sizeof(temp));
-    for (int i = 0; i < chicken.size(); i++) {
-        for (int j = 0; j < home.size(); j++) {
-            if (visited[i]) {
-                int a = abs(home[j].first - chicken[i].first) + abs(home[j].second - chicken[i].second);
-                temp[home[j].first][home[j].second] = min(temp[home[j].first][home[j].second], a);
-            }
-        }
-    }
-    int check = 0;
-    for (int i = 0; i < home.size(); i++) {
-        check += temp[home[i].first][home[i].second];
-    }
-    res = min(check, res);
+void combi(int start, vector<int> v) {
+	//m개 만큼 치킨집 골랐으면
+	if (v.size() == m) {
+		//고른 목록 chickenList에 push해주기
+		chickenList.push_back(v);
+		return;
+	}
+	//중복 피해야 하므로 start+1부터
+	for (int i = start + 1; i < chicken.size(); i++) {
+		//넣어주고
+		v.push_back(i);
+		//다음으로
+		combi(i, v);
+		//다른 경우도 생각해야하니 빼주기
+		v.pop_back();
+	}
+	return;
 }
-
-void dfs(int idx, int cnt) {
-    if (cnt == m) {
-        calc();
-        return;
-    }
-    for (int i = idx; i < chicken.size(); i++) {
-        if (visited[i]) continue;
-        visited[i] = true;
-        dfs(i+1,cnt + 1);
-        visited[i] = false;
-    }
-}
-
 
 int main() {
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> map[i][j];
-            if (map[i][j] == 1) home.push_back({ i,j });
-            if (map[i][j] == 2) chicken.push_back({ i,j });
-        }
-    }
-    dfs(0,0);
-    cout << res;
-    return 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 1) home.push_back({ i,j });
+			if (map[i][j] == 2) chicken.push_back({ i,j });
+		}
+	}
+	vector<int> v;
+	//치킨집 조합 짜기
+	combi(-1, v);
+	// 조합짠거를 기반으로 
+	for (vector<int> cList : chickenList) {
+		//도시의 치킨 거리 넣을 변수
+		int ret = 0;
+		//집의 좌표와 좌표수 만큼
+		for (pair<int, int> h : home) {
+			// 치킨 거리 넣을 변수
+			int temp = 1e9;
+			// 치킨집 조합 짠거로
+			for (int i : cList) {
+				// 치킨 거리 구해주고
+				int dist = abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second);
+				//해당 집에 대한 치킨거리 최소값을
+				temp = min(dist, temp);
+			}
+			//ret에 더해주고
+			ret += temp;
+		}
+		//최종 도시의 치킨거리의 최소 값 찾아주기
+		res = min(ret, res);
+	}
+	cout << res << '\n';
+	return 0;
 }
