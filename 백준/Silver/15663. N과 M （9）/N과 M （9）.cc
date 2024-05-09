3ba9215ded04294560bb4bd51fb32191f74b4b59
김건희 <92205960+kghees@users.noreply.github.com>
2024-05-09 15:50:08 +0800

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -8,6 +8,7 @@ int res[9];
 bool check[10] = { false };
 
 void dfs(int lev) {
+	//출력해야 될 수와 같아지면 출력
 	if (lev == m) {
 		for (int i = 0; i < m; i++) {
 			cout << res[i] << " ";
@@ -15,14 +16,17 @@ void dfs(int lev) {
 		cout << '\n';
 		return;
 	}
+	//그 전 수와 for문에서 같은지 비교해주기 위해 변수 하나 만듬
 	int temp = 0;
 	for (int i = 0; i < n; i++) {
+		//체크되지 않았고 재귀 돌아왔을 때 이전수와 현재수가 같지 않으면
 		if (!check[i] && temp != arr[i]) {
-			res[lev] = arr[i];
-			temp = res[lev];
-			check[i] = true;
-			dfs(lev + 1);
-			check[i] = false;
+			res[lev] = arr[i]; //결과 값 배열에 넣고
+			temp = res[lev]; //temp 새로운 수로 넣어주고
+			check[i] = true; // 방문 체크해주고
+			dfs(lev + 1); 
+			check[i] = false; //다음 탐색을 위해 방문 해제
+			res[lev] = 0;
 		}
 	}
 }
@@ -32,7 +36,8 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
+	//같은 숫자 전 인덱스와 배열하기 위해 sort
 	sort(arr, arr + n);
 	dfs(0);
 	return 0;
-}           
+}      
