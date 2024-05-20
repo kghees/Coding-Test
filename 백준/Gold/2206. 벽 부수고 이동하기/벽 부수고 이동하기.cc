315acd1e57c8539d529d8a531a3a704096cbf366
김건희 <92205960+kghees@users.noreply.github.com>
2024-05-20 17:12:53 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 104 ms, Memory: 15484 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -3,60 +3,50 @@
 #include <vector>
 #include <queue>
 using namespace std;
+struct wall {
+    int y, x;
+    int chance;
+};
 
 int n, m;
 int map[1001][1001];
-int dat[1001][1001];
+int dat[1001][1001][2];
 int dy[4] = { -1,1,0,0 };
 int dx[4] = { 0,0,-1,1 };
 int res;
 
-void bfs(int y, int x) {
-    queue<pair<int, int>> q1;
-    queue<pair<int, int>> q2;
-    q1.push({ y,x });
-    dat[y][x] = 1;
+int bfs() {
+    queue<wall> q;
+    q.push({ 0,0,0 }); //y,x좌표 및 벽 부순 여부
+    dat[0][0][0] = 1; //시작 점 1
 
-    while (!q1.empty()) {
-        int y = q1.front().first;
-        int x = q1.front().second;
-        q1.pop();
-
-        for (int k = 0; k < 4; k++) {
-            int ny = y + dy[k];
-            int nx = x + dx[k];
-
-            if (ny >= n || ny < 0 || nx >= m || nx < 0) continue;
-            if (dat[ny][nx]) continue;
-            dat[ny][nx] = dat[y][x] + 1;
-            if (map[ny][nx]) q2.push({ ny,nx });
-            else q1.push({ ny,nx });
-        }
-    }
-    res = dat[n - 1][m - 1];
-
-    while (!q2.empty()) {
-        int y = q2.front().first;
-        int x = q2.front().second;
-        q2.pop();
-        int next = dat[y][x] + 1;
+    while (!q.empty()) {
+        wall now = q.front();
+        q.pop();
+        //좌표 도달했으면 값 return
+        if (now.y == n - 1 && now.x == m - 1) return dat[now.y][now.x][now.chance];
 
         for (int k = 0; k < 4; k++) {
-            int ny = y + dy[k];
-            int nx = x + dx[k];
-
-            if (ny >= n || ny < 0 || nx >= m || nx < 0) continue;
-            if (map[ny][nx]) continue;
-
-            if (!dat[ny][nx] || next < dat[ny][nx]) {
-                dat[ny][nx] = next;
-                q2.push({ ny,nx });
+            int ny = now.y + dy[k];
+            int nx = now.x + dx[k];
+            //범위 안에 있고 
+            if (ny < n && ny >= 0 && nx < m && nx >= 0) {
+                //벽이 없고 아직 방문하지 않은 경우
+                if (!map[ny][nx] && !dat[ny][nx][now.chance]) {
+                    dat[ny][nx][now.chance] = dat[now.y][now.x][now.chance] + 1;
+                    q.push({ ny,nx,now.chance });
+                }
+                //벽인데 아직 벽 안부쉈다면
+                if (map[ny][nx] && now.chance == 0) {
+                    dat[ny][nx][now.chance + 1] = dat[now.y][now.x][now.chance] + 1;
+                    q.push({ ny,nx, now.chance + 1 });
+                }
             }
+
         }
     }
-    if (!res || res > dat[n - 1][m - 1]) {
-        res = dat[n - 1][m - 1];
-    }
+    //못갔으면 -1 return
+    return -1;
 }
 
 int main() {
@@ -68,9 +58,6 @@ int main() {
             map[i][j] = s[j] - '0';
         }
     }
-    bfs(0, 0);
-
-    if (!res) cout << -1;
-    else cout << res;
+    cout << bfs();
     return 0;
 }
